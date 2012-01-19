e10e4bd121c85b720615401ab820db83dc1c5b6f
Sanjay Agravat <sagrava@emory.edu>
2012-01-19 15:07:58 +0000

    space/indentation

--- eureka-webapp/src/main/java/edu/emory/cci/aiw/cvrg/eureka/servlet/LoginServlet.java
+++ eureka-webapp/src/main/java/edu/emory/cci/aiw/cvrg/eureka/servlet/LoginServlet.java
@@ -12,6 +12,7 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		
 		req.getRequestDispatcher("/index.jsp").forward(req, resp);
 	}
 }
