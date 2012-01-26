af966df1f31375a7d8fa4e6b13ba3b3a9ab38fd7
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-01-26 16:53:18 +0000

    Rearraged code to use proper DAOs when persisting entities, and abstracted the role and user creation.

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/config/Bootstrap.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/config/Bootstrap.java
@@ -1,12 +1,13 @@
 package edu.emory.cci.aiw.cvrg.eureka.services.config;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-
 import com.google.inject.Inject;
 
+import edu.emory.cci.aiw.cvrg.eureka.services.dao.RoleDao;
+import edu.emory.cci.aiw.cvrg.eureka.services.dao.UserDao;
 import edu.emory.cci.aiw.cvrg.eureka.services.entity.Role;
 import edu.emory.cci.aiw.cvrg.eureka.services.entity.User;
 
@@ -21,18 +22,28 @@ import edu.emory.cci.aiw.cvrg.eureka.services.entity.User;
 public class Bootstrap {
 
 	/**
-	 * The entity manager used to store test data in the data store.
+	 * The role DAO class.
 	 */
-	private final EntityManager entityManager;
+	private final RoleDao roleDao;
+	/**
+	 * The user DAO class.
+	 */
+	private final UserDao userDao;
 
 	/**
 	 * Create a Bootstrap class with an EntityManager.
 	 * 
+	 * @param inRoleDao The data access object to be used to work with user
+	 *            objects in the data store.
+	 * @param inUserDao The data access object to be used to work with role
+	 *            objects in the data store.
+	 * 
 	 * @param inEntityManager
 	 */
 	@Inject
-	Bootstrap(EntityManager inEntityManager) {
-		this.entityManager = inEntityManager;
+	Bootstrap(final RoleDao inRoleDao, final UserDao inUserDao) {
+		this.roleDao = inRoleDao;
+		this.userDao = inUserDao;
 	}
 
 	/**
@@ -48,27 +59,38 @@ public class Bootstrap {
 	 * Add the default roles to the data store.
 	 */
 	void addDefaultRoles() {
-		Role role1 = new Role();
-		role1.setDefaultRole(Boolean.TRUE);
-		role1.setName("researcher");
-
-		Role role2 = new Role();
-		role2.setDefaultRole(Boolean.FALSE);
-		role2.setName("admin");
-
-		this.entityManager.getTransaction().begin();
-		this.entityManager.persist(role1);
-		this.entityManager.persist(role2);
-		this.entityManager.getTransaction().commit();
+		for (Role role : Bootstrap.createRoles()) {
+			this.roleDao.save(role);
+		}
 	}
 
 	/**
 	 * Add the default users to the data store.
 	 */
 	void addDefaultUsers() {
-		List<Role> roles = this.entityManager.createQuery(
-				"select r from Role r where r.defaultRole = true", Role.class)
-				.getResultList();
+		List<Role> defaultRoles = new ArrayList<Role>();
+		for (Role role : this.roleDao.getRoles()) {
+			if (role.isDefaultRole() == Boolean.TRUE) {
+				defaultRoles.add(role);
+			}
+		}
+
+		for (User user : Bootstrap.createUsers(defaultRoles)) {
+			this.userDao.save(user);
+		}
+	}
+
+	/**
+	 * Create the users to be added to the data store.
+	 * 
+	 * @param defaultRoles The default roles to be assigned to each newly
+	 *            created user.
+	 * 
+	 * @return List of users to be added.
+	 */
+	private static List<User> createUsers(final List<Role> defaultRoles) {
+		System.out.println("DEFAULT ROLES: " + defaultRoles.size());
+		List<User> users = new ArrayList<User>();
 		User user = new User();
 		user.setActive(Boolean.TRUE);
 		user.setVerified(Boolean.TRUE);
@@ -78,10 +100,29 @@ public class Bootstrap {
 		user.setOrganization("Emory University");
 		user.setPassword("testpassword");
 		user.setLastLogin(Calendar.getInstance().getTime());
-		user.setRoles(roles);
+		user.setRoles(defaultRoles);
+		users.add(user);
+		return users;
+	}
 
-		this.entityManager.getTransaction().begin();
-		this.entityManager.persist(user);
-		this.entityManager.getTransaction().commit();
+	/**
+	 * Create the roles to be added to the data store.
+	 * 
+	 * @return List of roles to be added.
+	 */
+	private static List<Role> createRoles() {
+		List<Role> roles = new ArrayList<Role>();
+
+		Role role1 = new Role();
+		role1.setDefaultRole(Boolean.TRUE);
+		role1.setName("researcher");
+
+		Role role2 = new Role();
+		role2.setDefaultRole(Boolean.FALSE);
+		role2.setName("admin");
+
+		roles.add(role1);
+		roles.add(role2);
+		return roles;
 	}
 }
