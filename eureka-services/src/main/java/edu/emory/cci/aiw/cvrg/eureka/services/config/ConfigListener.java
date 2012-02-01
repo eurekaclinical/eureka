748d5afa871e115979c4fee31df812079ab286e3
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-02-01 00:21:40 +0000

    Moved creation of the background job update thread to the context listener class.

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/config/ConfigListener.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/config/ConfigListener.java
@@ -1,9 +1,16 @@
 package edu.emory.cci.aiw.cvrg.eureka.services.config;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.ServletContextEvent;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.persist.PersistService;
 import com.google.inject.servlet.GuiceServletContextListener;
+
+import edu.emory.cci.aiw.cvrg.eureka.services.thread.JobUpdateThread;
 
 /**
  * Set up the Guice dependency injection engine. Uses two modules:
@@ -14,6 +21,11 @@ import com.google.inject.servlet.GuiceServletContextListener;
  * 
  */
 public class ConfigListener extends GuiceServletContextListener {
+
+	/**
+	 * A thread that updates the status of all jobs.
+	 */
+	private JobUpdateThread jobUpdateThread;
 
 	@Override
 	protected Injector getInjector() {
@@ -31,5 +43,44 @@ public class ConfigListener extends GuiceServletContextListener {
 		bootstrap.configure();
 
 		return injector;
+	}
+
+	/*
+	 * @see
+	 * com.google.inject.servlet.GuiceServletContextListener#contextInitialized
+	 * (javax.servlet.ServletContextEvent)
+	 */
+	@Override
+	public void contextInitialized(ServletContextEvent inServletContextEvent) {
+		super.contextInitialized(inServletContextEvent);
+		String backendUrl = inServletContextEvent.getServletContext()
+				.getInitParameter("backend-url");
+		try {
+			this.jobUpdateThread = new JobUpdateThread(backendUrl);
+			this.jobUpdateThread.setDaemon(true);
+			this.jobUpdateThread.start();
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * @see
+	 * com.google.inject.servlet.GuiceServletContextListener#contextDestroyed
+	 * (javax.servlet.ServletContextEvent)
+	 */
+	@Override
+	public void contextDestroyed(ServletContextEvent inServletContextEvent) {
+		super.contextDestroyed(inServletContextEvent);
+		this.jobUpdateThread.setKeepRunning(false);
+		this.jobUpdateThread.interrupt();
+		try {
+			this.jobUpdateThread.join(20000);
+		} catch (InterruptedException e) {
+			// do nothing, we're about to shut down.
+			e.printStackTrace();
+		}
 	}
 }
