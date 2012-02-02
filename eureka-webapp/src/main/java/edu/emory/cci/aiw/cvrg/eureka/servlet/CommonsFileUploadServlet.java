826a0de5fd5df78ef4d4270f0de777d0d9052ca2
Sanjay Agravat <sagrava@emory.edu>
2012-02-02 17:57:55 +0000

    Removed reference to junit.

--- eureka-webapp/src/main/java/edu/emory/cci/aiw/cvrg/eureka/servlet/CommonsFileUploadServlet.java
+++ eureka-webapp/src/main/java/edu/emory/cci/aiw/cvrg/eureka/servlet/CommonsFileUploadServlet.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import junit.framework.Assert;
 
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
