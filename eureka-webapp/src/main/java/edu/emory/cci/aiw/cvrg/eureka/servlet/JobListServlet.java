b6f317bf25acaa3216e8f7fb07f27214c212cd6e
Sanjay Agravat <sagrava@emory.edu>
2012-02-04 22:37:50 +0000

    initial revision.

---
+++ eureka-webapp/src/main/java/edu/emory/cci/aiw/cvrg/eureka/servlet/JobListServlet.java
@@ -0,0 +1,46 @@
+package edu.emory.cci.aiw.cvrg.eureka.servlet;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.WebResource;
+
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.CommUtils;
+import edu.emory.cci.aiw.cvrg.eureka.common.entity.Job;
+
+public class JobListServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String eurekaServicesUrl = req.getSession().getServletContext()
+				.getInitParameter("eureka-services-url");
+		try {
+			Client client = CommUtils.getClient();
+			WebResource webResource = client.resource(eurekaServicesUrl);
+			List<Job> jobs = webResource.path("/api/job/list")
+					.accept(MediaType.APPLICATION_JSON)
+					.get(new GenericType<List<Job>>() {
+						// Nothing to implement, used to hold returned data.
+					});
+
+			req.setAttribute("jobs", jobs);
+			req.getRequestDispatcher("/tool.jsp").forward(req, resp);
+		} catch (NoSuchAlgorithmException nsae) {
+			throw new ServletException(nsae);
+		} catch (KeyManagementException kme) {
+			throw new ServletException(kme);
+		}
+	}
+}
