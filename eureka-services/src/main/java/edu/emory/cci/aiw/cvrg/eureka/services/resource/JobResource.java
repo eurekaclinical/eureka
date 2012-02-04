b60598c309c3de8dc229b93cc75ba78ee5f60ad0
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-02-04 03:25:35 +0000

    Updated the job submission method to use a thread instead of doing things synchronously.

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/resource/JobResource.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/resource/JobResource.java
@@ -1,10 +1,12 @@
 package edu.emory.cci.aiw.cvrg.eureka.services.resource;
 
-import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -19,19 +21,13 @@ import javax.ws.rs.core.SecurityContext;
 import com.google.inject.Inject;
 import com.sun.jersey.api.client.ClientResponse.Status;
 
-import edu.emory.cci.aiw.cvrg.eureka.common.entity.FileError;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.FileUpload;
-import edu.emory.cci.aiw.cvrg.eureka.common.entity.FileWarning;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.Job;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.User;
 import edu.emory.cci.aiw.cvrg.eureka.services.dao.FileDao;
 import edu.emory.cci.aiw.cvrg.eureka.services.dao.UserDao;
-import edu.emory.cci.aiw.cvrg.eureka.services.dataprovider.DataProvider;
-import edu.emory.cci.aiw.cvrg.eureka.services.dataprovider.DataProviderException;
-import edu.emory.cci.aiw.cvrg.eureka.services.dataprovider.XlsxDataProvider;
-import edu.emory.cci.aiw.cvrg.eureka.services.datavalidator.DataValidator;
-import edu.emory.cci.aiw.cvrg.eureka.services.datavalidator.ValidationEvent;
 import edu.emory.cci.aiw.cvrg.eureka.services.job.JobCollection;
+import edu.emory.cci.aiw.cvrg.eureka.services.thread.JobSubmissionThread;
 
 /**
  * REST operations related to jobs submitted by the user.
@@ -71,75 +67,35 @@ public class JobResource {
 	 * Create a new job (by uploading a new file)
 	 * 
 	 * @param fileUpload The file upload to add.
-	 * @return A {@link Status#CREATED} if the file is successfully added,
+	 * @param servletContext The servlet context, used for fetching context
+	 *            parameters out of the web.xml configuration file.
+	 * @return A {@link Status#OK} if the file is successfully added,
 	 *         {@link Status#BAD_REQUEST} if there are errors.
+	 * @throws ServletException
 	 */
-	@Path("/upload")
+	@Path("/add")
 	@POST
 	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response uploadFile(FileUpload fileUpload) {
-		// start with the assumption that the resource will be created.
-		Response response = Response.created(
-				URI.create("/" + fileUpload.getId())).build();
+	public Response uploadFile(FileUpload fileUpload,
+			@Context ServletContext servletContext) throws ServletException {
+		String configUrl = servletContext
+				.getInitParameter("backend-config-url");
+		String jobSubmitUrl = servletContext
+				.getInitParameter("backend-submission-url");
+
+		configUrl += "/" + fileUpload.getUser().getId();
 
 		try {
-			// first we validate the file upload.
-			DataProvider dataProvider = new XlsxDataProvider(fileUpload);
-			DataValidator dataValidator = new DataValidator();
-			dataValidator.setPatients(dataProvider.getPatients())
-					.setEncounters(dataProvider.getEncounters())
-					.setProviders(dataProvider.getProviders())
-					.setCpts(dataProvider.getCpts())
-					.setIcd9Procedures(dataProvider.getIcd9Procedures())
-					.setIcd9Diagnoses(dataProvider.getIcd9Diagnoses())
-					.setMedications(dataProvider.getMedications())
-					.setLabs(dataProvider.getLabs())
-					.setVitals(dataProvider.getVitals()).validate();
-			List<ValidationEvent> events = dataValidator.getValidationEvents();
-
-			// if the validation caused any errors/warnings, we insert them into
-			// our file upload object, and amend our response.
-			if (events.size() > 0) {
-				List<FileError> errors = new ArrayList<FileError>();
-				List<FileWarning> warnings = new ArrayList<FileWarning>();
-				for (ValidationEvent event : events) {
-					if (event.isFatal()) {
-						FileError error = new FileError();
-						error.setLineNumber(event.getLine());
-						error.setText(event.getMessage());
-						error.setType(event.getType());
-						error.setFileUpload(fileUpload);
-						errors.add(error);
-					} else {
-						FileWarning warning = new FileWarning();
-						warning.setLineNumber(event.getLine());
-						warning.setText(event.getMessage());
-						warning.setType(event.getType());
-						warning.setFileUpload(fileUpload);
-						warnings.add(warning);
-					}
-					fileUpload.setErrors(errors);
-					fileUpload.setWarnings(warnings);
-				}
-				response = Response.status(Status.BAD_REQUEST).entity(events)
-						.build();
-			} else {
-				// there are no errors, we send the job to the back end.
-				// TODO: SEND THE JOB TO THE BACK END!
-			}
-		} catch (DataProviderException e) {
-			response = Response.status(Status.BAD_REQUEST)
-					.entity(e.getMessage()).build();
+			JobSubmissionThread jobSubmissionThread = new JobSubmissionThread(
+					fileUpload, this.fileDao, configUrl, jobSubmitUrl);
+			jobSubmissionThread.start();
+		} catch (KeyManagementException e) {
+			throw new ServletException(e);
+		} catch (NoSuchAlgorithmException e) {
+			throw new ServletException(e);
 		}
-
-		// now we save the file upload off to the data base, so we can fetch it
-		// again if we need to process the data.
-		User user = this.userDao.get(fileUpload.getUser().getId());
-		fileUpload.setUser(user);
-		this.fileDao.save(fileUpload);
-
-		return response;
+		return Response.ok().build();
 	}
 
 	/**
