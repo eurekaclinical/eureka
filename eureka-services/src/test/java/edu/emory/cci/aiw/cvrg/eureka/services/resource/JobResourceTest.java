f3cd6e1b6988fb9129c92ce171cff39ca317de60
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-02-04 03:29:05 +0000

    A new test class to test jobs related functionality in JobResource class.

---
+++ eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/resource/JobResourceTest.java
@@ -0,0 +1,29 @@
+package edu.emory.cci.aiw.cvrg.eureka.services.resource;
+
+import org.junit.Test;
+
+/**
+ * Test cases for the job resource.
+ * 
+ * @author hrathod
+ * 
+ */
+public class JobResourceTest extends AbstractResourceTest {
+
+	/**
+	 * A test to check whether submitting a job via the JobResource class works.
+	 */
+	@Test
+	public void testJobSubmission() {
+		// TODO: activate this test when the back-end URLs are available.
+		// WebResource resource = this.resource();
+		// FileUpload fileUpload = new FileUpload();
+		// fileUpload.setLocation("/tmp/foo");
+		// User user = new User();
+		// user.setId(Long.valueOf(1));
+		// fileUpload.setUser(user);
+		// ClientResponse response = resource.path("/api/job/add").post(
+		// ClientResponse.class, fileUpload);
+		// System.out.println(response.getClientResponseStatus());
+	}
+}
