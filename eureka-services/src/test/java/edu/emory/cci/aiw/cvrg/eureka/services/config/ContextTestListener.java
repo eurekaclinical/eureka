29774c8ac575fd2bca64a8265777c445961d3abc
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-02-06 05:37:24 +0000

    Updated to match logic in the non-test version of the context listener.

--- eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/config/ContextTestListener.java
+++ eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/config/ContextTestListener.java
@@ -1,5 +1,7 @@
 package edu.emory.cci.aiw.cvrg.eureka.services.config;
 
+import javax.servlet.ServletContextEvent;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.persist.PersistService;
@@ -13,19 +15,47 @@ import com.google.inject.servlet.GuiceServletContextListener;
  */
 public class ContextTestListener extends GuiceServletContextListener {
 
+	/**
+	 * Make sure we always use the same injector
+	 */
+	private Injector injector = null;
+	/**
+	 * The persistence service for the application.
+	 */
+	private PersistService persistService = null;
+
 	@Override
-	protected Injector getInjector() {
-		Injector injector = Guice.createInjector(new ServletTestModule(),
-				new AppTestModule());
-
-		PersistService persistService = injector
-				.getInstance(PersistService.class);
-		persistService.start();
-
-		Bootstrap bootstrap = injector.getInstance(Bootstrap.class);
-		bootstrap.configure();
-
-		return injector;
+	synchronized protected Injector getInjector() {
+		if (this.injector == null) {
+			this.injector = Guice.createInjector(new ServletModule(),
+					new AppModule());
+		}
+		return this.injector;
 	}
 
+	/*
+	 * @see
+	 * com.google.inject.servlet.GuiceServletContextListener#contextInitialized
+	 * (javax.servlet.ServletContextEvent)
+	 */
+	@Override
+	public void contextInitialized(ServletContextEvent inServletContextEvent) {
+		super.contextInitialized(inServletContextEvent);
+		this.persistService = this.getInjector().getInstance(
+				PersistService.class);
+		this.persistService.start();
+		Bootstrap bootstrap = this.getInjector().getInstance(Bootstrap.class);
+		bootstrap.configure();
+	}
+
+	/*
+	 * @see
+	 * com.google.inject.servlet.GuiceServletContextListener#contextDestroyed
+	 * (javax.servlet.ServletContextEvent)
+	 */
+	@Override
+	public void contextDestroyed(ServletContextEvent inServletContextEvent) {
+		super.contextDestroyed(inServletContextEvent);
+		this.persistService.stop();
+	}
 }
