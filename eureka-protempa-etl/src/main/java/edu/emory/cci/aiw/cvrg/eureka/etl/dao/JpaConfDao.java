50a04c426d74604f95f19ce9235d03994e0fd172
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-02-13 16:05:43 +0000

    Added @Transactional annotation to the save() methods, and removed unnecessary warning suppressions.

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JpaConfDao.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JpaConfDao.java
@@ -6,30 +6,28 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
 
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.Configuration;
-
 
 public class JpaConfDao implements ConfDao {
 
 	private final EntityManager entityManager;
 
-
 	@Inject
-	public JpaConfDao (EntityManager manager) {
-
+	public JpaConfDao(EntityManager manager) {
 		this.entityManager = manager;
 	}
 
-	public void save (Configuration conf) {
-
-		this.entityManager.persist (conf);
+	@Transactional
+	public void save(Configuration conf) {
+		this.entityManager.persist(conf);
 	}
 
-	public Configuration get (Long id) {
-
-		Query query = this.entityManager.createQuery ("select c from Configuration c where c.id = ?1" , Configuration.class).setParameter(1 , id);
-		@SuppressWarnings("unchecked")
+	public Configuration get(Long id) {
+		Query query = this.entityManager.createQuery(
+				"select c from Configuration c where c.id = ?1",
+				Configuration.class).setParameter(1, id);
 		List<Configuration> resultList = query.getResultList();
 		return resultList.get(0);
 	}
--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JpaJobDao.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JpaJobDao.java
@@ -7,49 +7,45 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
 
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.Job;
 import edu.emory.cci.aiw.cvrg.eureka.etl.resource.JobFilter;
-
 
 public class JpaJobDao implements JobDao {
 
 	private final EntityManager entityManager;
 
 	@Inject
-	public JpaJobDao (EntityManager manager) {
-
+	public JpaJobDao(EntityManager manager) {
 		this.entityManager = manager;
 	}
 
-	public void save (Job job) {
-
-		this.entityManager.persist (job);
+	@Transactional
+	public void save(Job job) {
+		this.entityManager.persist(job);
 	}
 
-	public Job get (Long id) {
-
-		Query query = this.entityManager.createQuery ("select j from Job j where j.id = ?1" , Job.class).setParameter(1 , id);
-		@SuppressWarnings("unchecked")
+	public Job get(Long id) {
+		Query query = this.entityManager.createQuery(
+				"select j from Job j where j.id = ?1", Job.class).setParameter(
+				1, id);
 		List<Job> resultList = query.getResultList();
 		return resultList.get(0);
 	}
 
-    public List<Job> get (JobFilter jobFilter) {
+	public List<Job> get(JobFilter jobFilter) {
+		Query query = this.entityManager.createQuery("select j from Job j",
+				Job.class);
+		List<Job> resultList = query.getResultList();
+		List<Job> ret = new ArrayList<Job>(resultList.size());
 
-		Query query = this.entityManager.createQuery ("select j from Job j" , Job.class);
-        @SuppressWarnings ("unchecked")
-        List<Job> resultList = query.getResultList();
-        List<Job> ret = new ArrayList<Job>(resultList.size());
+		for (Job candidate : resultList) {
+			if (jobFilter.evaluate(candidate)) {
+				ret.add(candidate);
+			}
+		}
 
-        for (Job candidate : resultList) {
-
-        	if (jobFilter.evaluate (candidate)) {
-
-        		ret.add (candidate);
-        	}
-        }
-
-        return ret;
-    }
+		return ret;
+	}
 }
