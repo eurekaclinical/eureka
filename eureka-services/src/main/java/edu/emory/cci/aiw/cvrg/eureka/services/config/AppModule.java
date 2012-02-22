a445ccab590841fd58fb84c2537c4b0d5d85be66
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-02-22 22:53:48 +0000

    Updated to use the FreeMarker template generated emails rather than the simple implementation.

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/config/AppModule.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/config/AppModule.java
@@ -9,7 +9,7 @@ import edu.emory.cci.aiw.cvrg.eureka.services.dao.JpaUserDao;
 import edu.emory.cci.aiw.cvrg.eureka.services.dao.RoleDao;
 import edu.emory.cci.aiw.cvrg.eureka.services.dao.UserDao;
 import edu.emory.cci.aiw.cvrg.eureka.services.email.EmailSender;
-import edu.emory.cci.aiw.cvrg.eureka.services.email.EmailSenderImpl;
+import edu.emory.cci.aiw.cvrg.eureka.services.email.FreeMarkerEmailSender;
 
 /**
  * Configure all the non-web related binding for Guice.
@@ -24,7 +24,7 @@ public class AppModule extends AbstractModule {
 		bind(UserDao.class).to(JpaUserDao.class);
 		bind(RoleDao.class).to(JpaRoleDao.class);
 		bind(FileDao.class).to(JpaFileDao.class);
-		bind(EmailSender.class).to(EmailSenderImpl.class);
+		bind(EmailSender.class).to(FreeMarkerEmailSender.class);
 	}
 
 }
