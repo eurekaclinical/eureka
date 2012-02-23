063c81ac8be099a90e7ace56b88c07c3dc74b158
Sanjay Agravat <sagrava@emory.edu>
2012-02-23 06:05:15 +0000

    initial revision.

---
+++ eureka-webapp/src/main/java/edu/emory/rhsit/taglib/Util.java
@@ -0,0 +1,13 @@
+package edu.emory.rhsit.taglib;
+
+import java.util.Collection;
+
+public class Util {
+
+	
+	public static boolean contains(Collection<?> coll, Object o) {
+		
+		return coll.contains(o);
+
+	}
+}
