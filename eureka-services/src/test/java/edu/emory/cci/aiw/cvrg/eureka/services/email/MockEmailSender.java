4f908890eef6288bce5a0b5c89b8fd194001f349
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-02-24 19:30:30 +0000

    Updated to match the new EmailSender interface.

--- eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/email/MockEmailSender.java
+++ eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/email/MockEmailSender.java
@@ -11,7 +11,30 @@ import edu.emory.cci.aiw.cvrg.eureka.common.entity.User;
 public class MockEmailSender implements EmailSender {
 
 	@Override
-	public void sendMessage(User inUser) throws EmailException {
+	public void sendVerificationMessage(User inUser) throws EmailException {
+		// do nothing, we don't want any emails to actually be sent out.
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.emory.cci.aiw.cvrg.eureka.services.email.EmailSender#
+	 * sendActivationMessage(edu.emory.cci.aiw.cvrg.eureka.common.entity.User)
+	 */
+	@Override
+	public void sendActivationMessage(User inUser) throws EmailException {
+		// do nothing, we don't want any emails to actually be sent out.
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.emory.cci.aiw.cvrg.eureka.services.email.EmailSender#
+	 * sendPasswordChangeMessage
+	 * (edu.emory.cci.aiw.cvrg.eureka.common.entity.User)
+	 */
+	@Override
+	public void sendPasswordChangeMessage(User inUser) throws EmailException {
 		// do nothing, we don't want any emails to actually be sent out.
 	}
 
