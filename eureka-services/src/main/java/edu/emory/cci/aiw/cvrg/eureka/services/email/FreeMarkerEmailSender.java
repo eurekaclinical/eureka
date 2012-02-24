7518cad4df1175bdf209eae5b507853bff708698
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-02-24 19:28:44 +0000

    Updated to match the interface, the mail session is now injected along with the application properties, rather than fetching it in the class.

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/email/FreeMarkerEmailSender.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/email/FreeMarkerEmailSender.java
@@ -3,18 +3,21 @@ package edu.emory.cci.aiw.cvrg.eureka.services.email;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+
+import com.google.inject.Inject;
 
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.User;
+import edu.emory.cci.aiw.cvrg.eureka.services.config.ApplicationProperties;
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.Template;
@@ -36,25 +39,30 @@ public class FreeMarkerEmailSender implements EmailSender {
 	 * The mail session used to send the emails.
 	 */
 	private final Session session;
+	/**
+	 * The application configuration properties.
+	 */
+	private final ApplicationProperties applicationProperties;
 
 	/**
 	 * Default constructor, creates a FreeMarker configuration object.
 	 * 
+	 * @param inApplicationProperties The application configuration object.
+	 * @param inSession The mail session to use when sending a message.
+	 * 
 	 * @throws EmailException Thrown when the mail session can not be fetched
 	 *             properly from the JNDI context.
 	 */
-	public FreeMarkerEmailSender() throws EmailException {
+	@Inject
+	public FreeMarkerEmailSender(
+			final ApplicationProperties inApplicationProperties,
+			final Session inSession) throws EmailException {
+		this.applicationProperties = inApplicationProperties;
+		this.session = inSession;
 		this.configuration = new Configuration();
-		this.configuration.setClassForTemplateLoading(
-				FreeMarkerEmailSender.class, "/templates/");
+		this.configuration.setClassForTemplateLoading(this.getClass(),
+				"/templates/");
 		this.configuration.setObjectWrapper(new DefaultObjectWrapper());
-		try {
-			final Context initCtx = new InitialContext();
-			final Context envCtx = (Context) initCtx.lookup("java:comp/env");
-			this.session = (Session) envCtx.lookup("mail/Session");
-		} catch (NamingException ne) {
-			throw new EmailException(ne);
-		}
 	}
 
 	/*
@@ -65,30 +73,77 @@ public class FreeMarkerEmailSender implements EmailSender {
 	 * java.lang.String)
 	 */
 	@Override
-	public void sendMessage(final User inUser) throws EmailException {
-		Template template;
+	public void sendVerificationMessage(final User inUser)
+			throws EmailException {
+		sendMessage(inUser, "verification.ftl",
+				this.applicationProperties.getVerificationEmailSubject());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.emory.cci.aiw.cvrg.eureka.services.email.EmailSender#
+	 * sendActivationMessage(edu.emory.cci.aiw.cvrg.eureka.common.entity.User)
+	 */
+	@Override
+	public void sendActivationMessage(final User inUser) throws EmailException {
+		sendMessage(inUser, "activation.ftl",
+				this.applicationProperties.getActivationEmailSubject());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see edu.emory.cci.aiw.cvrg.eureka.services.email.EmailSender#
+	 * sendPasswordChangeMessage
+	 * (edu.emory.cci.aiw.cvrg.eureka.common.entity.User)
+	 */
+	@Override
+	public void sendPasswordChangeMessage(User inUser) throws EmailException {
+		sendMessage(inUser, "password.ftl",
+				this.applicationProperties.getPasswordChangeEmailSubject());
+	}
+
+	/**
+	 * Send an email to the given use, using contents generated from the given
+	 * template.
+	 * 
+	 * @param inUser The user to whom the message should be sent.
+	 * @param templateName The name of the template used to generate the
+	 *            contents of the email.
+	 * @param subject The subject for the email being sent.
+	 * @throws EmailException Thrown if there are any errors in generating
+	 *             content from the template, composing the email, or sending
+	 *             the email.
+	 */
+	private void sendMessage(final User inUser, final String templateName,
+			final String subject) throws EmailException {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("user", inUser);
+		params.put("config", this.applicationProperties);
+		Writer stringWriter = new StringWriter();
 		try {
-			template = this.configuration.getTemplate("verification.ftl");
-			Writer stringWriter = new StringWriter();
-			template.process(inUser, stringWriter);
-			String content = stringWriter.toString();
-			System.out.println(content);
-			MimeMessage message = new MimeMessage(this.session);
-			// message subject
-			message.setSubject("Eureka TESTING!");
-			// message body
+			Template template = this.configuration.getTemplate(templateName);
+			template.process(params, stringWriter);
+		} catch (TemplateException e) {
+			throw new EmailException(e);
+		} catch (IOException e) {
+			throw new EmailException(e);
+		}
+
+		String content = stringWriter.toString();
+		System.out.println(content);
+		MimeMessage message = new MimeMessage(this.session);
+		try {
+			message.setSubject(subject);
 			message.setContent(content, "text/plain");
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(
 					inUser.getEmail()));
 			Transport.send(message);
-
-		} catch (IOException ioe) {
-			throw new EmailException(ioe);
-		} catch (TemplateException te) {
-			throw new EmailException(te);
-		} catch (MessagingException me) {
-			throw new EmailException(me);
+		} catch (AddressException e) {
+			throw new EmailException(e);
+		} catch (MessagingException e) {
+			throw new EmailException(e);
 		}
 	}
-
 }
