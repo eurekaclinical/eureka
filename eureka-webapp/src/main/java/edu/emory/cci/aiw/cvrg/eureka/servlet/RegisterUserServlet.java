4da38a626b1b935ba7b699671c228cb21ad10027
Sanjay Agravat <sagrava@emory.edu>
2012-03-13 19:02:04 +0000

    updates for register user email validation.

--- eureka-webapp/src/main/java/edu/emory/cci/aiw/cvrg/eureka/servlet/RegisterUserServlet.java
+++ eureka-webapp/src/main/java/edu/emory/cci/aiw/cvrg/eureka/servlet/RegisterUserServlet.java
@@ -5,8 +5,6 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
@@ -17,6 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
@@ -24,12 +25,12 @@ import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.client.urlconnection.HTTPSProperties;
 
-import edu.emory.cci.aiw.cvrg.eureka.common.entity.Role;
-import edu.emory.cci.aiw.cvrg.eureka.common.entity.User;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.UserRequest;
 
 public class RegisterUserServlet extends HttpServlet {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(RegisterUserServlet.class);
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -70,8 +71,19 @@ public class RegisterUserServlet extends HttpServlet {
 					.accept(MediaType.TEXT_PLAIN)
 					.post(ClientResponse.class, userRequest);
 			
-			System.out.println("Registered: " + firstName + " " + lastName);
-
+			int status = response.getClientResponseStatus().getStatusCode();
+			if (status < HttpServletResponse.SC_BAD_REQUEST) {
+				resp.setStatus(status);				
+				resp.getWriter().close();
+			} else {
+				resp.setContentType("text/plain");
+				String msg = response.getEntity(String.class);
+				LOGGER.debug("Error: {}", msg);
+				resp.setStatus(status);
+				resp.setContentLength(msg.length());
+				resp.getWriter().write(msg);
+				resp.getWriter().close();
+			}
 			
 
 		} catch (KeyManagementException e) {
