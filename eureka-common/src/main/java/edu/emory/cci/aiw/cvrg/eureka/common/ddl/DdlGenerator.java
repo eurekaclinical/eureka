45584b5c47e06648c7e573e213dd2a0a04673758
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-03-27 21:09:09 +0000

    Some code fix ups.

--- eureka-common/src/main/java/edu/emory/cci/aiw/cvrg/eureka/common/ddl/DdlGenerator.java
+++ eureka-common/src/main/java/edu/emory/cci/aiw/cvrg/eureka/common/ddl/DdlGenerator.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Generates the SQL statements needed to create the tables used to persist the
@@ -18,19 +20,27 @@ import org.hibernate.tool.hbm2ddl.SchemaExport;
 public class DdlGenerator {
 
 	/**
+	 * The class level logger.
+	 */
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(DdlGenerator.class);
+	/**
+	 * The dialect to produce the SQL for.
+	 */
+	private static final String DIALECT = "org.hibernate.dialect.Oracle10gDialect";
+
+	/**
 	 * Given a list of classes annotated with the {@link Entity} annotation, a
 	 * Hibernate database dialect, and an output file name, generates the SQL
 	 * needed to persist the given entities to a database and writes the SQL out
 	 * to the given file.
 	 * 
 	 * @param classes A list of classes to generate SQL for.
-	 * @param dialect The dialect (database type) to tailor the SQL for.
 	 * @param outFile The location of the output file.
 	 */
-	private static void generate(List<Class<?>> classes, String dialect,
-			String outFile) {
+	private static void generate(List<Class<?>> classes, String outFile) {
 		final Configuration configuration = new Configuration();
-		configuration.setProperty("hibernate.dialect", dialect);
+		configuration.setProperty("hibernate.dialect", DIALECT);
 		configuration.setProperty("hibernate.hbm2ddl.auto", "create");
 
 		for (Class<?> c : classes) {
@@ -38,8 +48,7 @@ public class DdlGenerator {
 			if (annotation != null) {
 				configuration.addAnnotatedClass(c);
 			} else {
-				System.out.println("Class " + c.getName()
-						+ " is not annotated as an @Entity");
+				LOGGER.warn("{} is not annotated with @Entity", c.getName());
 			}
 		}
 
@@ -51,8 +60,10 @@ public class DdlGenerator {
 
 	/**
 	 * Generate the SQL for entities used in ETL layer classes.
+	 * 
+	 * @param outputFile The location of file where the SQL should be written.
 	 */
-	private static void generateBackendDdl() {
+	private static void generateBackendDdl(final String outputFile) {
 		final List<Class<?>> backendClasses = new ArrayList<Class<?>>();
 		backendClasses
 				.add(edu.emory.cci.aiw.cvrg.eureka.common.entity.Configuration.class);
@@ -60,14 +71,15 @@ public class DdlGenerator {
 				.add(edu.emory.cci.aiw.cvrg.eureka.common.entity.Job.class);
 		backendClasses
 				.add(edu.emory.cci.aiw.cvrg.eureka.common.entity.JobEvent.class);
-		generate(backendClasses, "org.hibernate.dialect.Oracle10gDialect",
-				"/tmp/test2.sql");
+		generate(backendClasses, outputFile);
 	}
 
 	/**
 	 * Generate the SQL for entities used in service layer classes.
+	 * 
+	 * @param outputFile The location of file where the SQL should be written.
 	 */
-	private static void generateServiceDdl() {
+	private static void generateServiceDdl(final String outputFile) {
 		final List<Class<?>> serviceClasses = new ArrayList<Class<?>>();
 		serviceClasses
 				.add(edu.emory.cci.aiw.cvrg.eureka.common.entity.User.class);
@@ -82,16 +94,23 @@ public class DdlGenerator {
 		serviceClasses
 				.add(edu.emory.cci.aiw.cvrg.eureka.common.entity.AbstractFileInfo.class);
 
-		generate(serviceClasses, "org.hibernate.dialect.Oracle10gDialect",
-				"/tmp/test.sql");
+		generate(serviceClasses, outputFile);
 	}
 
 	/**
 	 * @param args Command line arguments to the application. <b>Note:</b> these
-	 *            arguments are currently not utilized.
+	 *            arguments are currently not utilized. The first param is the
+	 *            file where the service layer DDL should go, and the second
+	 *            parameter is where the ETL layer DDL should go.
 	 */
 	public static void main(String[] args) {
-		generateServiceDdl();
-		generateBackendDdl();
+		if (args.length < 2) {
+			System.err.println("Please provide two parameters, first for "
+					+ "the service layer file and the second for "
+					+ "the ETL layer file.");
+		} else {
+			generateServiceDdl(args[0]);
+			generateBackendDdl(args[1]);
+		}
 	}
 }
