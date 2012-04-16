e60a95c927cca8f836a1dfa03af439701d07310a
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-04-16 15:36:22 +0000

    Updated location of the JobFilter class.  Fixed spelling error.

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JobDao.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JobDao.java
@@ -2,28 +2,28 @@ package edu.emory.cci.aiw.cvrg.eureka.etl.dao;
 
 import java.util.List;
 
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.JobFilter;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.Job;
-import edu.emory.cci.aiw.cvrg.eureka.etl.resource.JobFilter;
 
 /**
- * A data access object interface to retreive and store information about
+ * A data access object interface to retrieve and store information about
  * Protempa ETL jobs.
- * 
+ *
  * @author hrathod
- * 
+ *
  */
 public interface JobDao {
 
 	/**
 	 * Stores the give job to the data store.
-	 * 
+	 *
 	 * @param job The job to persist.
 	 */
 	public void save(Job job);
 
 	/**
 	 * Gets the job associated with the given unique identifier.
-	 * 
+	 *
 	 * @param id The unique identifier for the job.
 	 * @return The job associated with the give unique identifier.
 	 */
@@ -31,7 +31,7 @@ public interface JobDao {
 
 	/**
 	 * Gets a list of jobs that meet the given filter criteria.
-	 * 
+	 *
 	 * @param jobFilter The filter criteria.
 	 * @return A list of jobs that meet the filter criteria.
 	 */
@@ -40,7 +40,7 @@ public interface JobDao {
 	/**
 	 * Refreshes the give job by synchronizing the state of the object with the
 	 * latest data in the data store.
-	 * 
+	 *
 	 * @param inJob The job to refresh.
 	 */
 	void refresh(Job inJob);
