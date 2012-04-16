a68e081888e0964931e602cec035999a2ac2ee8d
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-04-16 15:32:16 +0000

    Moved from the Eureka Protempa ETL module to the common module, so that the filter can be sent from the service layer, instead of sending multiple request parameters.

---
+++ eureka-common/src/main/java/edu/emory/cci/aiw/cvrg/eureka/common/comm/JobFilter.java
@@ -0,0 +1,96 @@
+package edu.emory.cci.aiw.cvrg.eureka.common.comm;
+
+import java.util.Date;
+
+import edu.emory.cci.aiw.cvrg.eureka.common.entity.Job;
+
+public class JobFilter {
+
+	private final Long jobId;
+	private final Long userId;
+	private final String state;
+	private final Date from;
+	private final Date to;
+
+	private final byte score;
+
+	public JobFilter(Long jobId, Long userId, String state, Date from, Date to) {
+
+		this.jobId = jobId;
+		this.userId = userId;
+		this.state = state;
+		this.from = from;
+		this.to = to;
+
+		byte b = 0;
+		b |= (jobId != null) ? 1 : 0;
+		b |= (userId != null) ? 2 : 0;
+		b |= (state != null) ? 4 : 0;
+		b |= (from != null) ? 8 : 0;
+		b |= (to != null) ? 16 : 0;
+		score = b;
+	}
+
+	/**
+	 * @return the jobId
+	 */
+	public Long getJobId() {
+		return this.jobId;
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public Long getUserId() {
+		return this.userId;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return this.state;
+	}
+
+	/**
+	 * @return the from
+	 */
+	public Date getFrom() {
+		return this.from;
+	}
+
+	/**
+	 * @return the to
+	 */
+	public Date getTo() {
+		return this.to;
+	}
+
+	/**
+	 * @return the score
+	 */
+	public byte getScore() {
+		return this.score;
+	}
+
+	public boolean evaluate(Job j) {
+
+		if (score == 0) {
+
+			return true;
+		}
+
+		byte result = 0;
+		result |= (((score & 1) != 0) && this.jobId.equals(j.getId())) ? 1 : 0;
+		result |= (((score & 2) != 0) && this.userId.equals(j.getUserId())) ? 2
+				: 0;
+		result |= (((score & 4) != 0) && this.state.equals(j.getCurrentState())) ? 4
+				: 0;
+		result |= (((score & 8) != 0) && this.from.getTime() <= j
+				.getCreationTime().getTime()) ? 8 : 0;
+		result |= (((score & 16) != 0) && this.to.getTime() >= j
+				.getCreationTime().getTime()) ? 16 : 0;
+
+		return result == score;
+	}
+}
