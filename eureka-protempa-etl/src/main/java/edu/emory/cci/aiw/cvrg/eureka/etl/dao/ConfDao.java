ae63c2cec82f323768402ecd2fff3aa3f11ea402
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-04-23 14:05:09 +0000

    Added a getAll() method to get a list of all configurations.  Also changed construtor to accept a EntityManager provider, instead of an EntityManager itself, to make it thread-safe.

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/ConfDao.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/ConfDao.java
@@ -1,28 +1,37 @@
 package edu.emory.cci.aiw.cvrg.eureka.etl.dao;
 
+import java.util.List;
+
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.Configuration;
 
 /**
  * A data access object interface used to retrive and store information about
  * backend configurations.
- * 
+ *
  * @author hrathod
- * 
+ *
  */
 public interface ConfDao {
 
 	/**
 	 * Save the give configuration.
-	 * 
+	 *
 	 * @param conf The configuration to save.
 	 */
 	public void save(Configuration conf);
 
 	/**
 	 * Get a configuration for the give user.
-	 * 
+	 *
 	 * @param userId The unique identifier for the user.
 	 * @return The configuration belonging to the given user.
 	 */
 	public Configuration get(Long userId);
+
+	/**
+	 * Get all the configurations.
+	 *
+	 * @return all configurations.
+	 */
+	public List<Configuration> getAll();
 }
--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JpaConfDao.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JpaConfDao.java
@@ -1,9 +1,10 @@
 package edu.emory.cci.aiw.cvrg.eureka.etl.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.NonUniqueResultException;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -13,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.google.inject.persist.Transactional;
 
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.Configuration;
@@ -29,38 +31,65 @@ public class JpaConfDao implements ConfDao {
 	/**
 	 * The class level logger.
 	 */
-	private static final Logger LOGGER = LoggerFactory.getLogger(JpaConfDao.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(
+			JpaConfDao.class);
 	/**
 	 * The entity manager used to interact with the data store.
 	 */
-	private final EntityManager entityManager;
+	private final Provider<EntityManager> entityManagerProvider;
 
 	/**
 	 * Creates an object using the given entity manager.
 	 *
-	 * @param manager The entity manager to use for interaction with the data
-	 * store.
+	 * @param provider A provider that can deliver new entity manager instances
+	 * on-demand.
 	 */
 	@Inject
-	public JpaConfDao(EntityManager manager) {
-		this.entityManager = manager;
+	public JpaConfDao(Provider<EntityManager> provider) {
+		this.entityManagerProvider = provider;
+	}
+
+	/**
+	 * Returns a new entity manager to be used to interact with the data store.
+	 *
+	 * @return a new entity manager.
+	 */
+	private EntityManager getEntityManager() {
+		return this.entityManagerProvider.get();
 	}
 
 	@Override
 	@Transactional
 	public void save(Configuration conf) {
-		this.entityManager.persist(conf);
+		EntityManager entityManager = this.getEntityManager();
+		entityManager.persist(conf);
+		entityManager.flush();
+	}
+
+	@Override
+	public List<Configuration> getAll() {
+		EntityManager entityManager = this.getEntityManager();
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Configuration> criteriaQuery = builder.createQuery(
+				Configuration.class);
+		criteriaQuery.from(Configuration.class);
+		TypedQuery<Configuration> typedQuery = entityManager.createQuery(
+				criteriaQuery);
+		return typedQuery.getResultList();
 	}
 
 	@Override
 	public Configuration get(Long userId) {
 		Configuration configuration = null;
+		EntityManager entityManager = this.getEntityManager();
 		try {
-			CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
-			CriteriaQuery<Configuration> criteriaQuery = builder.createQuery(Configuration.class);
+			CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+			CriteriaQuery<Configuration> criteriaQuery = builder.createQuery(
+					Configuration.class);
 			Root<Configuration> root = criteriaQuery.from(Configuration.class);
-			TypedQuery<Configuration> query = this.entityManager.createQuery(criteriaQuery.
-					where(builder.equal(root.get(Configuration_.userId), userId)));
+			TypedQuery<Configuration> query = entityManager.createQuery(
+					criteriaQuery.where(builder.equal(root.get(
+					Configuration_.userId), userId)));
 			configuration = query.getSingleResult();
 		} catch (NoResultException nre) {
 			LOGGER.error(nre.getMessage(), nre);
