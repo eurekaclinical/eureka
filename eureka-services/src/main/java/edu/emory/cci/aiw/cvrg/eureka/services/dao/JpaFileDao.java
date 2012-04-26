8a969b098d802366965227e1e3cabb1e4defb04d
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-04-26 15:52:51 +0000

    Switched to using the QueryPathProvider interface to fetch the list of entities.

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/dao/JpaFileDao.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/dao/JpaFileDao.java
@@ -3,9 +3,7 @@ package edu.emory.cci.aiw.cvrg.eureka.services.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Root;
 
@@ -21,38 +19,37 @@ import edu.emory.cci.aiw.cvrg.eureka.common.entity.User_;
  * An implementation of {@link FileDao} interface using JPA entities.
  *
  * @author hrathod
- *
  */
-public class JpaFileDao extends GenericDao<FileUpload, Long> implements FileDao {
-
-	/**
-	 * The entity manager provider.
-	 */
-	private final Provider<EntityManager> emProvider;
+public class JpaFileDao extends GenericDao<FileUpload, Long>
+		implements FileDao {
 
 	/**
 	 * Construct an object with the given entity manager.
 	 *
 	 * @param inEMProvider The entity manager provider used to fetch an entity
-	 * manager to work with the data store.
-	 *
+	 *                     manager to work with the data store.
 	 */
 	@Inject
 	public JpaFileDao(Provider<EntityManager> inEMProvider) {
 		super(FileUpload.class, inEMProvider);
-		this.emProvider = inEMProvider;
 	}
 
+	/**
+	 * Gets all the file uploads that belong to the given user.
+	 * @param userId The unique identifier for the user.
+	 *
+	 * @return A list of file uploads belonging to the given user.
+	 */
 	@Override
 	public List<FileUpload> getByUserId(Long userId) {
-		EntityManager entityManager = this.getEntityManager();
-		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-		CriteriaQuery<FileUpload> criteriaQuery = builder.createQuery(
-				FileUpload.class);
-		Root<FileUpload> root = criteriaQuery.from(FileUpload.class);
-		Path<Long> path = root.get(FileUpload_.user).get(User_.id);
-		TypedQuery<FileUpload> query = entityManager.createQuery(criteriaQuery.
-				where(builder.equal(path, userId)));
-		return query.getResultList();
+		QueryPathProvider<FileUpload, Long> provider = new
+				QueryPathProvider<FileUpload, Long>() {
+			@Override
+			public Path<Long> getPath(Root<FileUpload> root,
+					CriteriaBuilder builder) {
+				return root.get(FileUpload_.user).get(User_.id);
+			}
+		};
+		return this.getListByAttribute(provider, userId);
 	}
 }
