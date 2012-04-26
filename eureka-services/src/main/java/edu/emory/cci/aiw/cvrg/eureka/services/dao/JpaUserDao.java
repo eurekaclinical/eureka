15944bef41fb59da0a1e8e7b4a2aa51e316059ed
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-04-26 15:54:10 +0000

    Cleaned up unused imports, removed unused commented out code.

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/dao/JpaUserDao.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/dao/JpaUserDao.java
@@ -1,12 +1,6 @@
 package edu.emory.cci.aiw.cvrg.eureka.services.dao;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Root;
-import javax.persistence.metamodel.SingularAttribute;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
@@ -20,7 +14,6 @@ import edu.emory.cci.aiw.cvrg.eureka.common.entity.User_;
  * and queries.
  *
  * @author hrathod
- *
  */
 public class JpaUserDao extends GenericDao<User, Long> implements UserDao {
 
@@ -28,7 +21,7 @@ public class JpaUserDao extends GenericDao<User, Long> implements UserDao {
 	 * Create an object with the give entity manager.
 	 *
 	 * @param inEMProvider The entity manager to be used for communication with
-	 * the data store.
+	 *                     the data store.
 	 */
 	@Inject
 	public JpaUserDao(Provider<EntityManager> inEMProvider) {
@@ -37,17 +30,7 @@ public class JpaUserDao extends GenericDao<User, Long> implements UserDao {
 
 	@Override
 	public User getByName(String name) {
-		User user = this.getUniqueByAttribute(User_.email, name);
-		return user;
-
-//		EntityManager entityManager = this.getEntityManager();
-//		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-//		CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
-//		Root<User> root = criteriaQuery.from(User.class);
-//		Path<String> path = root.get(User_.email);
-//		TypedQuery<User> typedQuery = entityManager.createQuery(criteriaQuery.
-//				where(builder.equal(path, name)));
-//		return typedQuery.getSingleResult();
+		return this.getUniqueByAttribute(User_.email, name);
 	}
 
 	@Override
