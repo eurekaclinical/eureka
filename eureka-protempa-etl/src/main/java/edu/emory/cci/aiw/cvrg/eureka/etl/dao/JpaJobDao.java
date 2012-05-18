e0ed4ceb24357fb6729da3da258eb8e664766f7a
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-05-18 20:27:33 +0000

    Added logging to find problems when creating query clauses out of job filter arguments.

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JpaJobDao.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/dao/JpaJobDao.java
@@ -55,32 +55,45 @@ public class JpaJobDao extends GenericDao<Job, Long> implements JobDao {
 		Root<Job> root = query.from(Job.class);
 		List<Predicate> predicates = new ArrayList<Predicate>();
 
+		LOGGER.debug("Checking for job ID.");
 		if (jobFilter.getJobId() != null) {
+			LOGGER.debug("Found job ID: {}", jobFilter.getJobId());
 			predicates.add(builder.equal(root.get(Job_.id),
 					jobFilter.getJobId()));
 		}
+		LOGGER.debug("Checking for user ID.");
 		if (jobFilter.getUserId() != null) {
+			LOGGER.debug("Found user ID: {}", jobFilter.getUserId());
 			predicates.add(builder.equal(root.get(Job_.userId),
 					jobFilter.getUserId()));
 		}
+		LOGGER.debug("Checking for start time.");
 		if (jobFilter.getFrom() != null) {
+			LOGGER.debug("Found start time: {}", jobFilter.getFrom());
 			predicates.add(builder.greaterThanOrEqualTo(
 					root.<Date>get(Job_.timestamp), jobFilter.getFrom()));
 		}
+		LOGGER.debug("Checking for end time.");
 		if (jobFilter.getTo() != null) {
+			LOGGER.debug("Found end time: {}", jobFilter.getTo());
 			predicates.add(builder.lessThanOrEqualTo(
 					root.<Date>get(Job_.timestamp), jobFilter.getTo()));
 		}
+		LOGGER.debug("Checking for state.");
 		if (jobFilter.getState() != null) {
+			LOGGER.debug("Found state: {}", jobFilter.getState());
 			predicates.add(builder.equal(
 					root.join(Job_.jobEvents).get(JobEvent_.state),
 					jobFilter.getState()));
 		}
 
+		LOGGER.debug("{} predicates found from filter", predicates.size());
 		Predicate[] predicatesArray = new Predicate[predicates.size()];
 		predicates.toArray(predicatesArray);
 		query.where(predicatesArray);
+		LOGGER.debug("Creating typed query.");
 		TypedQuery<Job> typedQuery = entityManager.createQuery(query);
+		LOGGER.debug("Returning results.");
 		return typedQuery.getResultList();
 	}
 }
