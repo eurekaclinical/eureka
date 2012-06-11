ca062a79f0ad1938c4c86de1c6c25f5b6acb3b04
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-06-11 19:09:34 +0000

    Added a new function to use the 'isUserInRole()' method of HttpServletRequest, rather than relying on the custom request object.

--- eureka-webapp/src/main/java/edu/emory/rhsit/taglib/Util.java
+++ eureka-webapp/src/main/java/edu/emory/rhsit/taglib/Util.java
@@ -1,13 +1,18 @@
 package edu.emory.rhsit.taglib;
 
 import java.util.Collection;
+import javax.servlet.http.HttpServletRequest;
 
 public class Util {
 
-	
+
 	public static boolean contains(Collection<?> coll, Object o) {
-		
+
 		return coll.contains(o);
 
+	}
+
+	public static boolean isUserInRole(HttpServletRequest request, String role) {
+		return request.isUserInRole(role);
 	}
 }
