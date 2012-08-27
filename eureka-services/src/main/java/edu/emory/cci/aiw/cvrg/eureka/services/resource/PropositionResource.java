c96a8c502a9f3124cef9fba41e9cc3627265b860
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-08-27 20:57:31 +0000

    Added a new method to validate propositions.  Currently always returns true.  A place holder until the ETL layer validation code is tested, at which time the entire workflow will be hooked up.

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/resource/PropositionResource.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/resource/PropositionResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -98,6 +99,15 @@ public class PropositionResource {
 			result.add(this.wrap(p));
 		}
 		return result;
+	}
+	
+	@POST
+	@Path("/user/validate/{userId}")
+	public Response validateProposition (@PathParam("userId") Long inUserId, 
+			PropositionWrapper inWrapper) {
+		Response response;
+		response = Response.ok().build();
+		return response;
 	}
 
 	@GET
