0fc27d046a70b015e7c9d7b361852b033a2a669c
Andrew Post <arpost@emory.edu>
2012-10-04 07:47:15 +0000

    Changed forward to logout.jsp to a redirect to make sure that the user principal is cleared out, otherwise the button bar continues to act as if the user is logged in.

--- eureka-webapp/src/main/java/edu/emory/cci/aiw/cvrg/eureka/servlet/LogoutServlet.java
+++ eureka-webapp/src/main/java/edu/emory/cci/aiw/cvrg/eureka/servlet/LogoutServlet.java
@@ -19,6 +19,6 @@ public class LogoutServlet extends HttpServlet {
 		if (session != null) {
 			session.invalidate();
 		}
-		req.getRequestDispatcher("/logout.jsp").forward(req, resp);
+                resp.sendRedirect(req.getContextPath() + "/logout.jsp");
 	}
 }
