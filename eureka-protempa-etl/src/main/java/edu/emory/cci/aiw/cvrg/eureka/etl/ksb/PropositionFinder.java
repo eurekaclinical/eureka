c42d768405e0c6134645850aa552f40cb9ce2bef
Andrew Post <arpost@emory.edu>
2012-10-05 01:48:02 +0000

    Use the default config file if it exists.

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/ksb/PropositionFinder.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/ksb/PropositionFinder.java
@@ -23,21 +23,52 @@ public class PropositionFinder {
 	private static final Logger LOGGER =
 			LoggerFactory.getLogger(PropositionFinder.class);
 	private static final File CONF_DIR = new File("/etc/eureka/etlconfig");
+
+	static {
+		if (LOGGER.isErrorEnabled()) {
+			if (!CONF_DIR.exists()) {
+				LOGGER.error(
+						"Configuration directory "
+						+ CONF_DIR.getAbsolutePath()
+						+ " does not exist. Proposition finding will not work without it. Please create it and try again.");
+			} else if (!CONF_DIR.isDirectory()) {
+				LOGGER.error(
+						"Path "
+						+ CONF_DIR.getAbsolutePath()
+						+ " is not a directory. Proposition finding requires it to be a directory.");
+			}
+		}
+	}
+	private static final File DEFAULT_CONF_DIR =
+			new File(CONF_DIR, "etlconfigdefaults");
 	private static final String CONF_PREFIX = "config";
+	private static final String DEFAULT_CONF_FILE = "defaults.ini";
 	private final KnowledgeSource knowledgeSource;
 
-	public PropositionFinder (Configuration inConfiguration) throws
-		PropositionFinderException {
+	public PropositionFinder(Configuration inConfiguration) throws
+			PropositionFinderException {
 		Long confId = inConfiguration.getId();
 		try {
 			String idStr = String.valueOf(confId.longValue());
 			String confFileName = CONF_PREFIX + idStr + ".ini";
 			Configurations configurations =
-				new INICommonsConfigurations(CONF_DIR);
+					new INICommonsConfigurations(CONF_DIR);
 			SourceFactory sf =
-				new SourceFactory(configurations,
+					new SourceFactory(configurations,
 					confFileName);
-			this.knowledgeSource = sf.newKnowledgeSourceInstance();
+			KnowledgeSource ks = sf.newKnowledgeSourceInstance();
+			if (ks == null && DEFAULT_CONF_DIR.exists()) {
+				File defaultConfFile = new File(DEFAULT_CONF_DIR,
+						DEFAULT_CONF_FILE);
+				if (defaultConfFile.exists()) {
+					Configurations defaultConfigs =
+							new INICommonsConfigurations(DEFAULT_CONF_DIR);
+					SourceFactory defaultSF = new SourceFactory(defaultConfigs,
+							DEFAULT_CONF_FILE);
+					ks = defaultSF.newKnowledgeSourceInstance();
+				}
+			}
+			this.knowledgeSource = ks;
 		} catch (BackendProviderSpecLoaderException e) {
 			throw new PropositionFinderException(e);
 		} catch (InvalidConfigurationException e) {
