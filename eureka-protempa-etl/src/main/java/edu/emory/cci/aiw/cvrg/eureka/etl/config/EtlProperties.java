6106deeb770f6b6fc546c9aef721b099d4afdde6
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-10-24 22:00:59 +0000

    Now subclasses ApplicationProperties.

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/config/EtlProperties.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/config/EtlProperties.java
@@ -19,12 +19,9 @@
  */
 package edu.emory.cci.aiw.cvrg.eureka.etl.config;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import com.google.inject.Singleton;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import edu.emory.cci.aiw.cvrg.eureka.common.props.ApplicationProperties;
 
 /**
  * Contains methods to fetch configuration information for the application.
@@ -32,72 +29,8 @@ import org.slf4j.LoggerFactory;
  * @author hrathod
  *
  */
-public class EtlProperties {
-
-	/**
-	 * The class level logger.
-	 */
-	private static final Logger LOGGER = LoggerFactory.getLogger(EtlProperties.class);
-	/**
-	 * Name of the properties file that is required for application
-	 * configuration.
-	 */
-	private static final String PROPERTIES_FILE = "/eureka-protempa-etl.properties";
-	/**
-	 * Holds an instance of the properties object which contains all the
-	 * application configuration properties.
-	 */
-	private final Properties properties;
-
-	/**
-	 * No-arg constructor, looks for the application configuration file and
-	 * loads up the properties.
-	 */
-	public EtlProperties() {
-		InputStream inputStream = this.getClass().getResourceAsStream(
-				PROPERTIES_FILE);
-
-		if (inputStream == null) {
-			throw new ConfigurationError(
-					"Missing application configuration file: "
-					+ PROPERTIES_FILE);
-		}
-
-		this.properties = new Properties();
-		try {
-			this.properties.load(inputStream);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		try {
-			inputStream.close();
-		} catch (IOException e) {
-			LOGGER.error(e.getMessage(), e);
-		}
-	}
-
-	/**
-	 * Gets the user-configured directory where the INI configuration files for
-	 * Protempa are located.
-	 *
-	 * @return A string containing the path to the directory containing Protempa
-	 * INI configuration files.
-	 */
-	public String getProtempaConfigDir() {
-		return this.properties.getProperty("eureka.etl.protempa.config.dir");
-	}
-
-	/**
-	 * Gets the user-configured directory where the i2b2 loader configuration
-	 * XML files are located.
-	 *
-	 * @return A string containing the path to the directory containing i2b2
-	 * loader XML configuration files.
-	 */
-	public String getI2b2ConfigDir() {
-		return this.properties.getProperty("eureka.etl.i2b2.config.dir");
-	}
+@Singleton
+public class EtlProperties extends ApplicationProperties {
 
 	/**
 	 * Gets the size of the thread pool created to run Protempa tasks.
@@ -106,26 +39,5 @@ public class EtlProperties {
 	 */
 	public int getTaskThreadPoolSize() {
 		return this.getIntValue("eureka.etl.threadpool.size", 4);
-	}
-
-	/**
-	 * Utility method to get an int from the properties file.
-	 *
-	 * @param propertyName The name of the property.
-	 * @param defaultValue The default value to return, if the property is not
-	 * found, or is malformed.
-	 * @return The property value, as an int.
-	 */
-	private int getIntValue(final String propertyName, int defaultValue) {
-		int result;
-		String property = this.properties.getProperty(propertyName);
-		try {
-			result = Integer.parseInt(property);
-		} catch (NumberFormatException e) {
-			LOGGER.warn("Invalid integer property in configuration: {}",
-					propertyName);
-			result = defaultValue;
-		}
-		return result;
 	}
 }
