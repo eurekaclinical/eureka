98722c666e22983d89c1718dabb7b5c2826d3a5a
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-11-13 15:24:41 +0000

    Removed unnecessary print statements.

--- eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/dao/PropositionDaoTest.java
+++ eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/dao/PropositionDaoTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -42,9 +42,5 @@ public class PropositionDaoTest extends AbstractServiceTest {
 		PropositionDao dao = this.getInstance(PropositionDao.class);
 		List<Proposition> propositions = dao.getAll();
 		Assert.assertEquals(3, propositions.size());
-
-		for (Proposition proposition : propositions) {
-			System.out.println(proposition.getUserId());
-		}
 	}
 }
