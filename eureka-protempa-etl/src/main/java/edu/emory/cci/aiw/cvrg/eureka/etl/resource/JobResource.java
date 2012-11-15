c69b7d5550fc65ca969bc7cbc6e9a4177514a852
Michel Mansour <michel.mansour@emory.edu>
2012-11-15 21:37:36 +0000

    switched from using PropositionWrapper to PropositionDefinition

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/resource/JobResource.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/resource/JobResource.java
@@ -20,7 +20,6 @@
 package edu.emory.cci.aiw.cvrg.eureka.etl.resource;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -33,8 +32,6 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.protempa.EventDefinition;
-import org.protempa.HighLevelAbstractionDefinition;
 import org.protempa.PropositionDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,21 +40,18 @@ import com.google.inject.Inject;
 
 import edu.emory.cci.aiw.cvrg.eureka.common.comm.JobFilter;
 import edu.emory.cci.aiw.cvrg.eureka.common.comm.JobRequest;
-import edu.emory.cci.aiw.cvrg.eureka.common.comm.PropositionWrapper;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.Configuration;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.Job;
 import edu.emory.cci.aiw.cvrg.eureka.etl.dao.ConfDao;
 import edu.emory.cci.aiw.cvrg.eureka.etl.dao.JobDao;
 import edu.emory.cci.aiw.cvrg.eureka.etl.job.TaskManager;
 import edu.emory.cci.aiw.cvrg.eureka.etl.validator.PropositionValidator;
-import edu.emory.cci.aiw.cvrg.eureka.etl.validator
-	.PropositionValidatorException;
-import org.protempa.ExtendedPropositionDefinition;
+import edu.emory.cci.aiw.cvrg.eureka.etl.validator.PropositionValidatorException;
 
 @Path("/job")
 public class JobResource {
-	private static final Logger LOGGER =
-		LoggerFactory.getLogger(JobResource.class);
+	private static final Logger LOGGER = LoggerFactory
+	        .getLogger(JobResource.class);
 	private final JobDao jobDao;
 	private final PropositionValidator propositionValidator;
 	private final TaskManager taskManager;
@@ -65,7 +59,7 @@ public class JobResource {
 
 	@Inject
 	public JobResource(JobDao inJobDao, TaskManager inTaskManager,
-		PropositionValidator inValidator, ConfDao inConfDao) {
+	        PropositionValidator inValidator, ConfDao inConfDao) {
 		this.jobDao = inJobDao;
 		this.taskManager = inTaskManager;
 		this.propositionValidator = inValidator;
@@ -93,39 +87,34 @@ public class JobResource {
 	public Response startJob(JobRequest inJobRequest) {
 		Response response;
 		Job job = inJobRequest.getJob();
-		List<PropositionWrapper> wrappers =
-			inJobRequest.getPropositionWrappers();
-		Configuration configuration = this.confDao.getByUserId(job.getUserId
-			());
+		List<PropositionDefinition> definitions = inJobRequest
+		        .getPropositions();
+		Configuration configuration = this.confDao.getByUserId(job.getUserId());
 		propositionValidator.setConfiguration(configuration);
-		propositionValidator.setPropositions(wrappers);
+		propositionValidator.setPropositions(definitions);
 		boolean valid;
 		try {
 			valid = propositionValidator.validate();
 		} catch (PropositionValidatorException e) {
-			LOGGER.error(e.getMessage(),e);
+			LOGGER.error(e.getMessage(), e);
 			valid = false;
 		}
 
 		if (valid) {
-			List<PropositionDefinition> definitions = this.unwrapAll
-				(wrappers);
 			LOGGER.debug("Created {} definitions", definitions.size());
 			job.setNewState("CREATED", null, null);
 			LOGGER.debug("Request to start new Job {}", job.getId());
 			this.jobDao.create(job);
 			this.taskManager.queueTask(job.getId(), definitions);
-			response = Response.created(URI.create("/" + job.getId()))
-				.build();
+			response = Response.created(URI.create("/" + job.getId())).build();
 		} else {
-			job.setNewState("FAILED",null,null);
+			job.setNewState("FAILED", null, null);
 			this.jobDao.create(job);
 			for (String message : propositionValidator.getMessages()) {
 				LOGGER.error(message);
 			}
-			response =
-				Response.status(Response.Status.BAD_REQUEST).entity
-					(propositionValidator.getMessages()).build();
+			response = Response.status(Response.Status.BAD_REQUEST)
+			        .entity(propositionValidator.getMessages()).build();
 		}
 		return response;
 	}
@@ -140,61 +129,8 @@ public class JobResource {
 		for (Job job : jobs) {
 			this.jobDao.refresh(job);
 			LOGGER.debug("Returning job {} with status {}", job.getId(),
-				job.getCurrentState());
+			        job.getCurrentState());
 		}
 		return jobs;
-	}
-
-	private List<PropositionDefinition> unwrapAll(List<PropositionWrapper>
-		inWrappers) {
-		List<PropositionDefinition> definitions =
-			new ArrayList<PropositionDefinition>();
-		if (inWrappers != null) {
-			for (PropositionWrapper wrapper : inWrappers) {
-				definitions.add(unwrap(wrapper));
-			}
-		}
-		return definitions;
-	}
-
-	private PropositionDefinition unwrap(PropositionWrapper inWrapper) {
-		PropositionDefinition definition;
-		String[] allTargets = getTargets(inWrapper.getChildren());
-
-		String idStr = String.valueOf(inWrapper.getId().longValue());
-		if (inWrapper.getType() == PropositionWrapper.Type.AND) {
-			HighLevelAbstractionDefinition d =
-				new HighLevelAbstractionDefinition(idStr);
-			d.setAbbreviatedDisplayName(inWrapper.getAbbrevDisplayName());
-			d.setDisplayName(inWrapper.getDisplayName());
-                        for (String target : allTargets) {
-                            d.add(new ExtendedPropositionDefinition(target));
-                        }
-			definition = d;
-		} else {
-			EventDefinition e = new EventDefinition(idStr);
-			e.setAbbreviatedDisplayName(inWrapper.getAbbrevDisplayName());
-			e.setDisplayName(inWrapper.getDisplayName());
-			e.setInverseIsA(allTargets);
-			definition = e;
-		}
-
-		return definition;
-	}
-
-	private String[] getTargets(List<PropositionWrapper> children) {
-		int size = children.size();
-		String[] result = new String[size];
-		int counter = 0;
-		for (PropositionWrapper wrapper : children) {
-			String id;
-			if (wrapper.isInSystem()) {
-				id = wrapper.getKey();
-			} else {
-				id = String.valueOf(wrapper.getId().longValue());
-			}
-			result[counter++] = id;
-		}
-		return result;
 	}
 }
