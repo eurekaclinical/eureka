5d010f5fc5d270a4193372d3f37e2264937b2002
Himanshu Rathod <himanshu.rathod@emory.edu>
2012-11-17 00:53:13 +0000

    Added some classes to make it easier to communicate between layers.

---
+++ eureka-common/src/main/java/edu/emory/cci/aiw/cvrg/eureka/common/comm/clients/AbstractClient.java
@@ -0,0 +1,44 @@
+/*
+ * #%L
+ * Eureka Common
+ * %%
+ * Copyright (C) 2012 Emory University
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package edu.emory.cci.aiw.cvrg.eureka.common.comm.clients;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.CommUtils;
+
+/**
+ * @author hrathod
+ */
+public abstract class AbstractClient {
+
+	protected AbstractClient() {
+	}
+
+	protected Client getRestClient() {
+		return CommUtils.getClient();
+	}
+
+	protected WebResource getResource () {
+		return this.getRestClient().resource(this.getResourceUrl());
+	}
+
+	abstract String getResourceUrl ();
+}
---
+++ eureka-common/src/main/java/edu/emory/cci/aiw/cvrg/eureka/common/comm/clients/ServicesClient.java
@@ -0,0 +1,63 @@
+/*
+ * #%L
+ * Eureka Common
+ * %%
+ * Copyright (C) 2012 Emory University
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package edu.emory.cci.aiw.cvrg.eureka.common.comm.clients;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.Sequence;
+import edu.emory.cci.aiw.cvrg.eureka.common.entity.User;
+
+/**
+ * @author hrathod
+ */
+public class ServicesClient extends AbstractClient {
+
+	private static final GenericType<User> UserType = new GenericType<User>
+		() {
+	};
+	private final String servicesUrl;
+
+	public ServicesClient(String inServicesUrl) {
+		super();
+		this.servicesUrl = inServicesUrl;
+	}
+
+	@Override
+	String getResourceUrl() {
+		return this.servicesUrl;
+	}
+
+	public User getUserByName(String username) {
+		final String path = "/api/user/byname/" + username;
+		return this.getResource().path(path).get(UserType);
+	}
+
+	public void saveSequence(Sequence inSequence) throws ClientException {
+		final String path = "/user/create/sequence";
+		ClientResponse response = this.getResource().path(path).post
+			(ClientResponse.class, inSequence);
+		if (!response.getClientResponseStatus().equals(
+			ClientResponse.Status.OK)) {
+			String message = response.getEntity(String.class);
+			throw new ClientException(message);
+		}
+	}
+}
