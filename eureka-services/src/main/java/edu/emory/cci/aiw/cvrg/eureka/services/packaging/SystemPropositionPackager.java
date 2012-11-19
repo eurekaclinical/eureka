852ef785a06d24eef13462cd499856b08e885279
Michel Mansour <michel.mansour@emory.edu>
2012-11-19 04:05:38 +0000

    "creates" PropositionDefinition by retrieving them from the system via SystemPropositionFinder

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/packaging/SystemPropositionPackager.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/packaging/SystemPropositionPackager.java
@@ -19,37 +19,26 @@
  */
 package edu.emory.cci.aiw.cvrg.eureka.services.packaging;
 
-import org.protempa.ConstantDefinition;
-import org.protempa.EventDefinition;
-import org.protempa.HighLevelAbstractionDefinition;
-import org.protempa.LowLevelAbstractionDefinition;
-import org.protempa.PrimitiveParameterDefinition;
 import org.protempa.PropositionDefinition;
-import org.protempa.SliceDefinition;
 
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.SystemProposition;
+import edu.emory.cci.aiw.cvrg.eureka.services.config.ServiceProperties;
+import edu.emory.cci.aiw.cvrg.eureka.services.finder.SystemPropositionFinder;
+import edu.emory.cci.aiw.cvrg.eureka.services.finder.SystemPropositionRetriever;
 
 public final class SystemPropositionPackager implements
         PropositionDefinitionPackager<SystemProposition, PropositionDefinition> {
 
+	private final Long userId;
+	
+	public SystemPropositionPackager(Long inUserId) {
+		this.userId = inUserId;
+	}
+	
 	@Override
 	public PropositionDefinition pack(SystemProposition proposition) {
-		switch (proposition.getSystemType()) {
-			case EVENT:
-				return new EventDefinition(proposition.getKey());
-			case CONSTANT:
-				return new ConstantDefinition(proposition.getKey());
-			case PRIMITIVE_PARAMETER:
-				return new PrimitiveParameterDefinition(proposition.getKey());
-			case HIGH_LEVEL_ABSTRACTION:
-				return new HighLevelAbstractionDefinition(proposition.getKey());
-			case LOW_LEVEL_ABSTRACTION:
-				return new LowLevelAbstractionDefinition(proposition.getKey());
-			case SLICE_ABSTRACTION:
-				return new SliceDefinition(proposition.getKey());
-			default:
-				return null;
-		}
+		SystemPropositionFinder finder = new SystemPropositionFinder(
+		        new SystemPropositionRetriever(new ServiceProperties()));
+			return finder.find(this.userId, proposition.getKey());
 	}
-
 }
