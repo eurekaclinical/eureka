c7a8e8989913a623448a307cf20116bb2cbef3bf
Michel Mansour <michel.mansour@emory.edu>
2012-11-19 04:16:55 +0000

    replaced uses of PropositionWrapper with DataElement

--- eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/resource/PropositionResourceTest.java
+++ eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/resource/PropositionResourceTest.java
@@ -23,46 +23,46 @@ import java.util.List;
 
 import javax.ws.rs.core.MediaType;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
 
-import edu.emory.cci.aiw.cvrg.eureka.common.comm.PropositionWrapper;
-
-import junit.framework.Assert;
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.DataElement;
 
 public class PropositionResourceTest extends AbstractServiceResourceTest {
 
 	private static final Long USER_ID = 1L;
-	private static final GenericType<List<PropositionWrapper>>
-		wrapperListType = new GenericType<List<PropositionWrapper>>(){};
+	private static final GenericType<List<DataElement>> elementListType = new GenericType<List<DataElement>>() {
+	};
 
-	private List<PropositionWrapper> getUserPropositions(Long inUserId) {
+	private List<DataElement> getUserPropositions(Long inUserId) {
 		WebResource resource = this.resource();
-		return resource.path("/api/proposition/user/list/" + inUserId).accept
-			(MediaType.APPLICATION_JSON).get(wrapperListType);
+		return resource.path("/api/proposition/user/list/" + inUserId)
+		        .accept(MediaType.APPLICATION_JSON).get(elementListType);
 	}
 
 	@Test
 	public void userPropositionTest() {
-		List<PropositionWrapper> wrappers = this.getUserPropositions(USER_ID);
-		Assert.assertTrue(wrappers.size() > 0);
+		List<DataElement> elements = this.getUserPropositions(USER_ID);
+		Assert.assertTrue(elements.size() > 0);
 	}
 
 	@Test
-	public void deletePropositionTest () {
-		List<PropositionWrapper> wrappers = this.getUserPropositions(USER_ID);
-		PropositionWrapper target = wrappers.get(0);
-		ClientResponse response = this.resource().path
-			("/api/proposition/user/delete/" + USER_ID + "/" + target.getId()
-			).delete(ClientResponse.class);
+	public void deletePropositionTest() {
+		List<DataElement> elements = this.getUserPropositions(USER_ID);
+		DataElement target = elements.get(0);
+		ClientResponse response = this
+		        .resource()
+		        .path("/api/proposition/user/delete/" + USER_ID + "/"
+		                + target.getId()).delete(ClientResponse.class);
 		Assert.assertEquals(response.getClientResponseStatus(),
-			ClientResponse.Status.OK);
+		        ClientResponse.Status.OK);
 
-		List<PropositionWrapper> afterDelete = this.getUserPropositions
-			(USER_ID);
-		Assert.assertEquals(wrappers.size() - 1, afterDelete.size());
+		List<DataElement> afterDelete = this.getUserPropositions(USER_ID);
+		Assert.assertEquals(elements.size() - 1, afterDelete.size());
 	}
 }
