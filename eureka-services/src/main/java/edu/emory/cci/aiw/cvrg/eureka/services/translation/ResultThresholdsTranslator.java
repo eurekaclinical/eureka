8b5c5e5a5709e446646a7a864d45bdef7e331fa7
Michel Mansour <michel.mansour@emory.edu>
2012-12-03 20:11:47 +0000

    building SimpleParameterConstraints

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/translation/ResultThresholdsTranslator.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/translation/ResultThresholdsTranslator.java
@@ -22,30 +22,67 @@ package edu.emory.cci.aiw.cvrg.eureka.services.translation;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.inject.Inject;
+
 import edu.emory.cci.aiw.cvrg.eureka.common.comm.ResultThresholds;
 import edu.emory.cci.aiw.cvrg.eureka.common.comm.ValueThreshold;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.LowLevelAbstraction;
+import edu.emory.cci.aiw.cvrg.eureka.common.entity.LowLevelAbstraction.CreatedFrom;
+import edu.emory.cci.aiw.cvrg.eureka.common.entity.Proposition;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.SimpleParameterConstraint;
+import edu.emory.cci.aiw.cvrg.eureka.services.dao.PropositionDao;
+import edu.emory.cci.aiw.cvrg.eureka.services.dao.TimeUnitDao;
+import edu.emory.cci.aiw.cvrg.eureka.services.dao.ValueComparatorDao;
 
 public final class ResultThresholdsTranslator implements
         PropositionTranslator<ResultThresholds, LowLevelAbstraction> {
 
+	private Long userId;
+	private final PropositionDao propositionDao;
+	private final TimeUnitDao timeUnitDao;
+	private final ValueComparatorDao valueCompDao;
+
+	@Inject
+	public ResultThresholdsTranslator(PropositionDao inPropositionDao,
+	        TimeUnitDao inTimeUnitDao, ValueComparatorDao inValueComparatorDao) {
+		propositionDao = inPropositionDao;
+		timeUnitDao = inTimeUnitDao;
+		valueCompDao = inValueComparatorDao;
+	}
+
+	public void setUserId(Long inUserId) {
+		userId = inUserId;
+	}
+
 	@Override
 	public LowLevelAbstraction translateFromElement(ResultThresholds element) {
 		LowLevelAbstraction result = new LowLevelAbstraction();
-		
-		PropositionTranslatorUtil.populateCommonPropositionFields(result, element);
+
+		PropositionTranslatorUtil.populateCommonPropositionFields(result,
+		        element);
+		List<Proposition> abstractedFrom = new ArrayList<Proposition>();
 		List<SimpleParameterConstraint> spcs = new ArrayList<SimpleParameterConstraint>();
 		for (ValueThreshold vt : element.getValueThresholds()) {
+			abstractedFrom.add(propositionDao.getByUserAndKey(userId, vt.getDataElementKey()));
 			spcs.add(buildSimpleParameterConstraint(vt));
 		}
+		result.setAbstractedFrom(abstractedFrom);
 		result.setSimpleParameterConstraints(spcs);
-		
+		result.setCreatedFrom(CreatedFrom.VALUE_THRESHOLD);
+
 		return result;
 	}
-	
+
 	private SimpleParameterConstraint buildSimpleParameterConstraint(ValueThreshold threshold) {
 		SimpleParameterConstraint result = new SimpleParameterConstraint();
+
+		result.setMinValueThreshold(threshold.getLowerValue());
+		result.setMinValueComp(valueCompDao.retrieve(threshold.getLowerComp()));
+		result.setMinUnits(threshold.getLowerUnits());
+		
+		result.setMaxValueThreshold(threshold.getUpperValue());
+		result.setMaxValueComp(valueCompDao.retrieve(threshold.getUpperComp()));
+		result.setMaxUnits(threshold.getUpperUnits());
 		
 		return result;
 	}
