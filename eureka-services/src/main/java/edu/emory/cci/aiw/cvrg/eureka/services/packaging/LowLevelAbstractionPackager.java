c6afc07c4a701e4f416a6068bb8be2d8139de4b7
Michel Mansour <michel.mansour@emory.edu>
2012-12-26 05:14:33 +0000

    implemented packaging

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/packaging/LowLevelAbstractionPackager.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/packaging/LowLevelAbstractionPackager.java
@@ -19,22 +19,71 @@
  */
 package edu.emory.cci.aiw.cvrg.eureka.services.packaging;
 
-import org.protempa.LowLevelAbstractionDefinition;
-
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.LowLevelAbstraction;
+import edu.emory.cci.aiw.cvrg.eureka.common.entity.Proposition;
+import edu.emory.cci.aiw.cvrg.eureka.common.entity.SimpleParameterConstraint;
+import org.protempa.LowLevelAbstractionDefinition;
+import org.protempa.LowLevelAbstractionValueDefinition;
+import org.protempa.proposition.value.NumberValue;
+import org.protempa.proposition.value.ValueComparator;
+
+import static edu.emory.cci.aiw.cvrg.eureka.services.packaging.PropositionDefinitionPackagerUtil.unit;
 
 public final class LowLevelAbstractionPackager
         implements
         PropositionDefinitionPackager<LowLevelAbstraction, LowLevelAbstractionDefinition> {
+
+	static final String COMPLEMENT_SUFFIX = "_COMPLEMENT";
+
 
 	@Override
 	public LowLevelAbstractionDefinition pack(LowLevelAbstraction proposition) {
 		LowLevelAbstractionDefinition result = new LowLevelAbstractionDefinition(
 		        proposition.getId().toString());
 
-		
-		
+		for (Proposition p : proposition.getAbstractedFrom()) {
+			result.addPrimitiveParameterId(p.getKey());
+		}
+
+		constraintToValueDefinition(proposition.getKey(),
+				proposition.getUserConstraint(), result);
+		constraintToValueDefinition(proposition.getKey() + COMPLEMENT_SUFFIX,
+				proposition.getComplementConstraint(), result);
+
+		result.setMinimumNumberOfValues(proposition.getMinValues());
+		result.setMinimumGapBetweenValues(proposition.getMinGapValues());
+		result.setMinimumGapBetweenValuesUnits(unit(proposition
+				.getMinGapValuesUnits()));
+		result.setMaximumGapBetweenValues(proposition.getMaxGapValues());
+		result.setMinimumGapBetweenValuesUnits(unit(proposition
+				.getMaxGapValuesUnits()));
+
 		return result;
+	}
+
+	private static void
+			constraintToValueDefinition(String name,
+												SimpleParameterConstraint
+												constraint,
+										LowLevelAbstractionDefinition def) {
+		LowLevelAbstractionValueDefinition valueDef = new
+				LowLevelAbstractionValueDefinition(def, name);
+		if (constraint.getMinValueThreshold() != null && constraint
+				.getMinValueComp() != null) {
+			valueDef.setParameterValue("minThreshold",
+					NumberValue.getInstance(constraint.getMinValueThreshold()
+							.longValue()));
+			valueDef.setParameterComp("minThreshold", ValueComparator.parse
+					(constraint.getMinValueComp().getName()));
+		}
+		if (constraint.getMaxValueThreshold() != null && constraint
+				.getMaxValueComp() != null) {
+			valueDef.setParameterValue("maxThreshold",
+					NumberValue.getInstance(constraint.getMaxValueThreshold()
+							.longValue()));
+			valueDef.setParameterComp("maxThreshold", ValueComparator.parse
+					(constraint.getMaxValueComp().getName()));
+		}
 	}
 
 }
