39a7141e5c8fce81bd0cd99b4feeafc1f98f901c
Michel Mansour <michel.mansour@emory.edu>
2012-12-26 04:57:45 +0000

    classes for representing compound low-level abstractions

---
+++ eureka-common/src/main/java/edu/emory/cci/aiw/cvrg/eureka/common/entity/ValueDefinitionMatchOperator.java
@@ -0,0 +1,71 @@
+/*
+ * #%L
+ * Eureka Common
+ * %%
+ * Copyright (C) 2012 Emory University
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package edu.emory.cci.aiw.cvrg.eureka.common.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+*
+*/
+@Entity
+@Table(name = "value_def_match_op")
+public class ValueDefinitionMatchOperator {
+
+	@Id
+	@SequenceGenerator(name = "VALUE_DEF_MATCH_OP_SEQ_GENERATOR",
+			sequenceName =  "VALUE_DEF_MATCH_OP_SEQ", allocationSize = 1,
+			initialValue = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+			generator = "VALUE_DEF_MATCH_OP_SEQ_GENERATOR")
+	private Long id;
+
+	private String name;
+
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
