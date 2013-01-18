778eef92b89275b5cf25e138ba6292949cd799b6
Andrew Post <arpost@emory.edu>
2013-01-18 23:57:35 +0000

    Fixed bug in data element editing when a data element's name (and hence its key) is changed.

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/translation/TranslatorSupport.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/translation/TranslatorSupport.java
@@ -40,76 +40,83 @@ import edu.emory.cci.aiw.cvrg.eureka.services.util.PropositionUtil;
  * @author Andrew Post
  */
 final class TranslatorSupport {
+
 	private final PropositionDao propositionDao;
 	private final SystemPropositionFinder finder;
 
 	@Inject
-	public TranslatorSupport(PropositionDao propositionDao, 
+	public TranslatorSupport(PropositionDao propositionDao,
 			SystemPropositionFinder finder) {
 		this.propositionDao = propositionDao;
 		this.finder = finder;
 	}
-	
+
 	/**
 	 * Retrieves the user or system entity if it exists, otherwise creates an
 	 * entity from a system proposition with with the given user id and key.
-	 * 
+	 *
 	 * @param userId
 	 * @param key
 	 * @return
-	 * @throws DataElementHandlingException 
+	 * @throws DataElementHandlingException
 	 */
-	DataElementEntity getSystemEntityInstance(Long userId, String key) 
+	DataElementEntity getSystemEntityInstance(Long userId, String key)
 			throws DataElementHandlingException {
 		DataElementEntity abstractedFrom =
-				propositionDao.getByUserAndKey(userId,key);
+				propositionDao.getByUserAndKey(userId, key);
 		if (abstractedFrom == null) {
 			try {
 				PropositionDefinition propDef = this.finder.find(userId, key);
-				abstractedFrom = PropositionUtil.toSystemProposition(propDef, 
+				abstractedFrom = PropositionUtil.toSystemProposition(propDef,
 						userId);
 			} catch (PropositionFindException ex) {
 				throw new DataElementHandlingException(
 						Response.Status.PRECONDITION_FAILED,
-						"No system entity with name " + key, ex);
+						"No system data element with name " + key, ex);
 			}
 		}
 		return abstractedFrom;
 	}
-	
+
 	/**
-	 * Creates a new user entity if one with the provided data element's user 
-	 * id and key does not already exist.
-	 * 
+	 * Creates a new user entity if one with the provided data element's id
+	 * does not already exist.
+	 *
 	 * @param <P>
 	 * @param element
 	 * @param cls
-	 * @return 
+	 * @return
+	 * @throws DataElementHandlingException if there already exists a system
+	 * data element with the same key as the candidate user data element.
 	 */
-	<P extends DataElementEntity> P getUserEntityInstance(DataElement element, 
-			Class<P> cls) {
+	<P extends DataElementEntity> P getUserEntityInstance(DataElement element,
+			Class<P> cls) throws DataElementHandlingException {
 		String key;
 		if (element.getKey() != null) {
 			key = element.getKey();
 		} else {
 			key = element.getAbbrevDisplayName();
 		}
-		
+
 		Date now = new Date();
 
 		P result;
-		DataElementEntity oldEntity = propositionDao.getByUserAndKey(
-				element.getUserId(), key);
+		DataElementEntity oldEntity;
+		if (element.getId() != null) {
+			oldEntity = propositionDao.retrieve(element.getId());
+		} else {
+			oldEntity = null;
+		}
 		if (cls.isInstance(oldEntity)) {
 			result = cls.cast(oldEntity);
 		} else {
 			try {
 				result = cls.newInstance();
 			} catch (InstantiationException ex) {
-				throw new AssertionError("Could not instantiate entity " + key 
+				throw new AssertionError("Could not instantiate entity " + key
 						+ ": " + ex.getMessage());
 			} catch (IllegalAccessException ex) {
-				throw new AssertionError("Could not instantiate entity " + key 
+				throw new AssertionError("Could not instantiate entity " + key
 						+ ": " + ex.getMessage());
 			}
 			result.setCreated(now);
@@ -117,24 +124,22 @@ final class TranslatorSupport {
 
 		result.setInSystem(false);
 		result.setLastModified(now);
-		
+
 		populateCommonEntityFields(result, element);
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * Populates the fields common to all propositions based on the given
 	 * proposition.
-	 * 
-	 * @param entity
-	 *            the {@link DataElementEntity} to populate. Modified as a result of
-	 *            calling this method.
-	 * @param dataElement
-	 *            the {@link DataElement} to get the data from
+	 *
+	 * @param entity the {@link DataElementEntity} to populate. Modified as a
+	 * result of calling this method.
+	 * @param dataElement the {@link DataElement} to get the data from
 	 */
 	private static void populateCommonEntityFields(DataElementEntity entity,
-	        DataElement dataElement) {
+			DataElement dataElement) {
 		entity.setId(dataElement.getId());
 		entity.setDisplayName(dataElement.getDisplayName());
 		entity.setAbbrevDisplayName(dataElement.getAbbrevDisplayName());
