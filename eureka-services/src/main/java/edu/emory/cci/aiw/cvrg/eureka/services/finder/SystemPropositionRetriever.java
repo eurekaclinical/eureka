534077c5c53e6a252c8d8dbe6b193a463efb66fa
Michel Mansour <michel.mansour@emory.edu>
2013-02-20 03:58:08 +0000

    added method to retrieve a list of proposition definitions

--- eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/finder/SystemPropositionRetriever.java
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/finder/SystemPropositionRetriever.java
@@ -19,6 +19,9 @@
  */
 package edu.emory.cci.aiw.cvrg.eureka.services.finder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.protempa.PropositionDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,32 +37,58 @@ import edu.emory.cci.aiw.cvrg.eureka.services.config.ServiceProperties;
  * 
  * @author hrathod
  */
-public class SystemPropositionRetriever implements PropositionRetriever<Long,
-		String> {
+public class SystemPropositionRetriever implements
+        PropositionRetriever<Long, String> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger
-			(SystemPropositionRetriever.class);
+	private static final Logger LOGGER = LoggerFactory
+	        .getLogger(SystemPropositionRetriever.class);
 	private final ServiceProperties applicationProperties;
 
 	@Inject
 	public SystemPropositionRetriever(ServiceProperties inProperties) {
 		this.applicationProperties = inProperties;
 	}
-	
+
 	@Override
-	public PropositionDefinition retrieve(Long inUserId, String inKey) 
-			throws PropositionFindException {
+	public PropositionDefinition retrieve(Long inUserId, String inKey)
+	        throws PropositionFindException {
 		EtlClient etlClient = new EtlClient(
-				this.applicationProperties.getEtlUrl());
+		        this.applicationProperties.getEtlUrl());
 		PropositionDefinition result;
 		try {
 			result = etlClient.getPropositionDefinition(inUserId, inKey);
 		} catch (ClientException e) {
 			LOGGER.error(e.getMessage(), e);
 			throw new PropositionFindException(
-					"Could not retrieve proposition definition " + inKey + 
-					" for user " + inUserId, e);
+			        "Could not retrieve proposition definition " + inKey
+			                + " for user " + inUserId, e);
 		}
 		return result;
+	}
+
+	/**
+	 * Retrieves all of the system elements given by the keys for the given user
+	 * 
+	 * @param inUserId the user ID
+	 * @param inKeys the keys of the system elements to retrieve
+	 * @param withChildren whether the children of the given elements should be retrieved as well
+	 * @return a {@link List} of {@link SystemElement}s
+	 * @throws PropositionFindException
+	 */
+	public List<PropositionDefinition> retrieveAll(Long inUserId,
+	        List<String> inKeys, Boolean withChildren) throws PropositionFindException {
+		EtlClient etlClient = new EtlClient(
+		        this.applicationProperties.getEtlUrl());
+		List<PropositionDefinition> result = new ArrayList<PropositionDefinition>();
+		try {
+			result = etlClient.getPropositionDefinitions(inUserId, inKeys, withChildren);
+		} catch (ClientException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new PropositionFindException(
+			        "Could not retrieve proposition definitions for user "
+			                + inUserId, e);
+		}
+		return result;
+
 	}
 }
