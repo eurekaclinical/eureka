7ae2edff56241d450bfd5ae94d5ec6848c74de7e
Andrew Post <arpost@emory.edu>
2013-04-27 01:51:50 +0000

    Major changes to Eureka to implement direct database support.

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/resource/PingResource.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/resource/PingResource.java
@@ -1,6 +1,6 @@
 /*
  * #%L
- * Eureka Protempa ETL
+ * Eureka Services
  * %%
  * Copyright (C) 2012 - 2013 Emory University
  * %%
@@ -19,111 +19,37 @@
  */
 package edu.emory.cci.aiw.cvrg.eureka.etl.resource;
 
-import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import com.google.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 
-import edu.emory.cci.aiw.cvrg.eureka.common.entity.Configuration;
-import edu.emory.cci.aiw.cvrg.eureka.common.exception.HttpStatusException;
-import edu.emory.cci.aiw.cvrg.eureka.etl.config.EtlProperties;
-import edu.emory.cci.aiw.cvrg.eureka.etl.dao.ConfDao;
-import edu.emory.cci.aiw.i2b2etl.configuration.ConfigurationReadException;
-import edu.emory.cci.aiw.i2b2etl.configuration.ConfigurationReader;
-import edu.emory.cci.aiw.i2b2etl.configuration.DatabaseSection;
 
 /**
+ * A REST resource to allow an administrator to test an account.
+ *
  * @author hrathod
  */
 @Path("/ping")
 @Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class PingResource {
 
-	private final ConfDao confDao;
-	private final EtlProperties etlProperties;
-
 	@Inject
-	public PingResource(ConfDao inConfDao, EtlProperties inEtlProperties) {
-		this.confDao = inConfDao;
-		this.etlProperties = inEtlProperties;
+	public PingResource() {
 	}
-
+	
 	@GET
-	@Path("/{id}")
-	public String ping(@PathParam("id") Long inUserId) {
-		String response = "Ping successful";
-		Configuration configuration = this.confDao.getByUserId(inUserId);
-		if (configuration == null) {
-			throw new HttpStatusException(
-					Response.Status.NOT_FOUND,
-					"No configuration found for user " + inUserId);
-		} else {
-			try {
-				testProtempaDatabase(configuration);
-			} catch (SQLException e) {
-				throw new HttpStatusException(
-						Response.Status.PRECONDITION_FAILED,
-						"Error connecting to Protempa " +
-								"database for user " + inUserId);
-			}
-			try {
-				testI2b2Database(configuration);
-			} catch (SQLException e) {
-				throw new HttpStatusException(
-						Response.Status.PRECONDITION_FAILED,
-						"Error connecting to i2b2 database for user " + inUserId);
-			} catch (ConfigurationReadException e) {
-				throw new HttpStatusException(
-						Response.Status.PRECONDITION_FAILED,
-						"Error reading i2b2 configuration for user " + inUserId);
-			}
-		}
-		return response;
-	}
-
-	private void testProtempaDatabase(Configuration inConfiguration)
-			throws SQLException {
-		String url = inConfiguration.getProtempaJdbcUrl();
-		Connection connection = DriverManager.getConnection(
-				url, inConfiguration.getProtempaSchema(),
-				inConfiguration.getProtempaPassword());
-		connection.close();
-	}
-
-	private void testI2b2Database(Configuration inConfiguration)
-			throws ConfigurationReadException, SQLException {
-		File configDirectory = new File(
-				this.etlProperties.getConfigDir(), "etlconfig");
-		File confFile = new File(
-				configDirectory, "config" + inConfiguration.getId() + ".xml");
-		ConfigurationReader configurationReader = new ConfigurationReader(
-				confFile);
-		configurationReader.read();
-		DatabaseSection databaseSection = configurationReader
-				.getDatabaseSection();
-
-		DatabaseSection.DatabaseSpec dataschema = databaseSection
-				.get("dataschema");
-		Connection dataConnection = DriverManager.getConnection(
-				dataschema.connect, dataschema.user, dataschema.passwd);
-		dataConnection.close();
-
-		DatabaseSection.DatabaseSpec metaschema = databaseSection
-				.get("metaschema");
-		Connection metaConnection = DriverManager.getConnection(
-				metaschema.connect, metaschema.user, metaschema.passwd);
-		metaConnection.close();
+	@Path("")
+	public Response doPing(@Context HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		NewCookie cookie = new NewCookie("JSESSIONID", session.getId());
+		return Response.ok().cookie(cookie).build();
 	}
 }
