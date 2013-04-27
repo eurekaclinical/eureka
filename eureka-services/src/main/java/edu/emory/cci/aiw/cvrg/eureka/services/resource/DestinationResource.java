9641b5b3657e139b07ff07fa9b285a370ef485b5
Andrew Post <arpost@emory.edu>
2013-04-27 01:25:33 +0000

    Major changes to Eureka to implement direct database support.

---
+++ eureka-services/src/main/java/edu/emory/cci/aiw/cvrg/eureka/services/resource/DestinationResource.java
@@ -0,0 +1,86 @@
+/*
+ * #%L
+ * Eureka Services
+ * %%
+ * Copyright (C) 2012 - 2013 Emory University
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package edu.emory.cci.aiw.cvrg.eureka.services.resource;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.inject.Inject;
+import com.sun.jersey.api.client.ClientResponse;
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.Destination;
+
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.SystemElement;
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.clients.ClientException;
+import edu.emory.cci.aiw.cvrg.eureka.common.exception.HttpStatusException;
+import edu.emory.cci.aiw.cvrg.eureka.services.config.EtlClient;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ * @author Andrew Post
+ */
+@Path("/destination")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class DestinationResource {
+
+	private final EtlClient etlClient;
+
+	@Inject
+	public DestinationResource(EtlClient inEtlClient) {
+		this.etlClient = inEtlClient;
+	}
+
+	/**
+	 * Gets all of the system elements for a user
+	 *
+	 * @param inUserId the user ID
+	 * @return a {@link List} of {@link SystemElement}s
+	 */
+	@GET
+	@Path("/list")
+	public List<Destination> getAll() {
+		try {
+			return this.etlClient.getDestinations();
+		} catch (ClientException ex) {
+			throw new HttpStatusException(Status.INTERNAL_SERVER_ERROR, ex);
+		}
+	}
+
+	@GET
+	@Path("/{id}")
+	public Destination get(@PathParam("id") String inId) {
+		try {
+			return this.etlClient.getDestination(inId);
+		} catch (ClientException ex) {
+			if (ex.getResponseStatus() == ClientResponse.Status.NOT_FOUND) {
+				throw new HttpStatusException(Status.NOT_FOUND);
+			} else {
+				throw new HttpStatusException(Status.INTERNAL_SERVER_ERROR, ex);
+			}
+		}
+	}
+
+}
---
+++ eureka-services/src/test/java/edu/emory/cci/aiw/cvrg/eureka/services/config/MockEtlClient.java
@@ -0,0 +1,114 @@
+package edu.emory.cci.aiw.cvrg.eureka.services.config;
+
+/*
+ * #%L
+ * Eureka Services
+ * %%
+ * Copyright (C) 2012 - 2013 Emory University
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.Destination;
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.Job;
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.JobFilter;
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.JobRequest;
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.SourceConfig;
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.ValidationRequest;
+import edu.emory.cci.aiw.cvrg.eureka.common.comm.clients.ClientException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+import org.protempa.PropositionDefinition;
+
+/**
+ *
+ * @author Andrew Post
+ */
+public class MockEtlClient implements EtlClient {
+
+	@Override
+	public Destination getDestination(String destId) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public List<Destination> getDestinations() throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Job getJob(Long inJobId) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public List<Job> getJobStatus(JobFilter inFilter) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public List<Job> getJobs() throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public PropositionDefinition getPropositionDefinition(String sourceConfigId, String inKey) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public List<PropositionDefinition> getPropositionDefinitions(String sourceConfigId, List<String> inKeys, Boolean withChildren) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public SourceConfig getSourceConfig(String sourceConfigId) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public List<SourceConfig> getSources() throws ClientException {
+		SourceConfig mockSourceConfig = new SourceConfig();
+		mockSourceConfig.setId("mockSourceConfig");
+		mockSourceConfig.setDisplayName("Mock Source Configuration");
+		return Collections.singletonList(mockSourceConfig);
+	}
+
+	@Override
+	public void ping(Long inUserId) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public Long submitJob(JobRequest inJobRequest) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public void upload(String fileName, String sourceId, String fileTypeId, InputStream inputStream) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public void validatePropositions(ValidationRequest inRequest) throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public List<Job> getJobsDesc() throws ClientException {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+	
+}
