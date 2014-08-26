3bf8bab005ddd98e9c2ebf639249ade43b2a28d2
Andrew Post <arpost@emory.edu>
2014-08-26 18:13:22 -0400

    Fixed issue with I2B2 destination naming. We now use the filename as the name and ignore the displayName tag. Using the displayName tag is incompatible with the new name field in the database.

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/resource/I2B2DestinationsDTOExtractor.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/resource/I2B2DestinationsDTOExtractor.java
@@ -19,28 +19,11 @@ package edu.emory.cci.aiw.cvrg.eureka.etl.resource;
  * limitations under the License.
  * #L%
  */
-
-import edu.emory.cci.aiw.cvrg.eureka.common.comm.EtlCohortDestination;
-import edu.emory.cci.aiw.cvrg.eureka.common.comm.EtlDestination;
 import edu.emory.cci.aiw.cvrg.eureka.common.comm.EtlI2B2Destination;
-import edu.emory.cci.aiw.cvrg.eureka.common.entity.CohortDestinationEntity;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.EtlUserEntity;
 import edu.emory.cci.aiw.cvrg.eureka.common.entity.I2B2DestinationEntity;
-import edu.emory.cci.aiw.cvrg.eureka.common.exception.HttpStatusException;
 import edu.emory.cci.aiw.cvrg.eureka.etl.config.EtlProperties;
 import edu.emory.cci.aiw.cvrg.eureka.etl.dao.EtlGroupDao;
-import java.io.IOException;
-import javax.ws.rs.core.Response;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 /**
  *
@@ -48,50 +31,27 @@ import org.xml.sax.SAXException;
  */
 class I2B2DestinationsDTOExtractor extends DestinationsDTOExtractor<EtlI2B2Destination, I2B2DestinationEntity> {
 
-	private final XPathExpression displayNameExpr;
-	private final DocumentBuilderFactory docBuilderFactory;
-	private final EtlProperties etlProperties;
-
 	public I2B2DestinationsDTOExtractor(EtlProperties inEtlProperties, EtlUserEntity user, EtlGroupDao inGroupDao) {
 		super(user, inGroupDao);
-
-		XPathFactory xpathFactory = XPathFactory.newInstance();
-		XPath xpath = xpathFactory.newXPath();
-		try {
-			displayNameExpr = xpath.compile("/queryResultsHandler/displayName/text()");
-		} catch (XPathExpressionException ex) {
-			throw new AssertionError("Invalid xpath expression: " + ex.getMessage());
-		}
-		this.docBuilderFactory = DocumentBuilderFactory.newInstance();
-		this.docBuilderFactory.setNamespaceAware(true); // never forget this!
-
-		this.etlProperties = inEtlProperties;
 	}
 
 	@Override
 	EtlI2B2Destination extractDTO(Perm perm,
 			I2B2DestinationEntity destinationEntity) {
-		try {
-			DocumentBuilder builder = this.docBuilderFactory.newDocumentBuilder();
-			EtlI2B2Destination dest = new EtlI2B2Destination();
-			Document doc = builder.parse(this.etlProperties.destinationConfigFile(destinationEntity.getName()));
-			String displayName = (String) displayNameExpr.evaluate(doc, XPathConstants.STRING);
-			dest.setName(displayName);
+		EtlI2B2Destination dest = new EtlI2B2Destination();
+		dest.setName(destinationEntity.getName());
 
-			dest.setId(destinationEntity.getId());
+		dest.setId(destinationEntity.getId());
 
-			dest.setRead(perm.read);
-			dest.setWrite(perm.write);
-			dest.setExecute(perm.execute);
+		dest.setRead(perm.read);
+		dest.setWrite(perm.write);
+		dest.setExecute(perm.execute);
 
-			dest.setOwnerUserId(destinationEntity.getOwner().getId());
-			
-			dest.setCreatedAt(destinationEntity.getCreatedAt());
-			dest.setUpdatedAt(destinationEntity.getUpdatedAt());
+		dest.setOwnerUserId(destinationEntity.getOwner().getId());
 
-			return dest;
-		} catch (XPathExpressionException | ParserConfigurationException | IOException | SAXException ex) {
-			throw new HttpStatusException(Response.Status.INTERNAL_SERVER_ERROR, ex);
-		}
+		dest.setCreatedAt(destinationEntity.getCreatedAt());
+		dest.setUpdatedAt(destinationEntity.getUpdatedAt());
+
+		return dest;
 	}
 }
