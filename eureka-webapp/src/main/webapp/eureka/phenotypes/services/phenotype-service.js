0fb2a2221bb53c8e972c70f4442d4b422baf4ff7
JsonS <jsands79@gmail.com>
2015-09-21 16:10:56 -0400

    Refactoring changes moving items to service.

--- eureka-webapp/src/main/webapp/eureka/phenotypes/services/phenotype-service.js
+++ eureka-webapp/src/main/webapp/eureka/phenotypes/services/phenotype-service.js
@@ -19,12 +19,42 @@
     function PhenotypeService($http, $q) {
 
         return ({
-            getSummarizedUserElements: getSummarizedUserElements
+            getSummarizedUserElements: getSummarizedUserElements,
+            getPhenotypeMessages: getPhenotypeMessages
         });
 
         function getSummarizedUserElements() {
             return $http.get('/eureka-services/api/protected/dataelement?summarize=true')
                 .then(handleSuccess, handleError);
+        }
+
+        function getPhenotypeMessages(){
+            return {
+                'CATEGORIZATION': {
+                    'displayName': 'Category',
+                    'description': `For defining a significant category of codes or clinical events or observations.`
+                },
+                'TEMPORAL': {
+                    'displayName': 'Temporal',
+                    'description': `For defining a disease, finding or patient care process to be reflected by codes
+                    ,clinical events and/or observations in a specified frequency, sequential or other temporal 
+                    patterns.`
+                },
+                'SEQUENCE': {
+                    'displayName': 'Sequence',
+                    'description': `For defining a disease, finding or patient care process to be reflected by codes,
+                    clinical events and/or observations in a specified sequential temporal pattern.`
+                },
+                'FREQUENCY': {
+                    'displayName': 'Frequency',
+                    'description': `For defining a disease, finding or patient care process to be reflected by codes,
+                    clinical events and/or observations in a specified frequency.`
+                },
+                'VALUE_THRESHOLD': {
+                    'displayName': 'Value Threshold',
+                    'description': `For defining clinically significant thresholds on the value of an observation.`
+                }
+            };
         }
 
         function handleSuccess(response) {
