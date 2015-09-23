c649a742dfbf0bfa03b71a6769fb64e939012841
Joel Kanzelmeyer <kanzelm3@gmail.com>
2015-09-22 20:59:39 -0400

    Updated test runner

--- eureka-webapp/src/main/webapp/eureka/phenotypes/services/phenotype-service-spec.js
+++ eureka-webapp/src/main/webapp/eureka/phenotypes/services/phenotype-service-spec.js
@@ -3,18 +3,41 @@
 
     describe('PhenotypeService', function() {
 
-        var PhenotypeService;
+        var PhenotypeService, $httpBackend;
 
         beforeEach(function() {
             module('eureka.phenotypes');
         });
 
-        beforeEach(inject(function(_PhenotypeService_) {
+        beforeEach(inject(function(_PhenotypeService_, _$httpBackend_) {
             PhenotypeService = _PhenotypeService_;
+            $httpBackend = _$httpBackend_;
         }));
 
         it('should be defined', function() {
             expect(PhenotypeService).toBeDefined();
         });
+
+        describe('getSummarizedUserElements', function() {
+
+            var userElements;
+
+            beforeEach(function () {
+                userElements = {
+                    blah: 'test'
+                };
+                $httpBackend.expectGET('/eureka-services/api/protected/dataelement?summarize=true')
+                    .respond(userElements);
+            });
+
+            it('should get the user elements', function() {
+                PhenotypeService.getSummarizedUserElements().then(function(res) {
+                    expect(res).toEqual(userElements);
+                });
+                $httpBackend.flush();
+            });
+
+        });
+
     });
 }());
