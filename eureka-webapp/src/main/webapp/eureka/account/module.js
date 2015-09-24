30bb037663279407615dafdc03f292889449e467
JsonS <jsands79@gmail.com>
2015-09-24 18:13:49 -0400

    stubbing out account

---
+++ eureka-webapp/src/main/webapp/eureka/account/module.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    /**
+     * @ngdoc overview
+     * @name eureka.account
+     * @description
+     * The module for the account section of the Eureka application.
+     */
+    angular.module('eureka.account', []);
+}());
