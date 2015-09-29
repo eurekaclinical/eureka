07ca8d2c9558b94ae38031407248b4d3e1ed0d75
JsonS <jsands79@gmail.com>
2015-09-28 21:28:39 -0400

    added in change password modal.  Still more to do

--- eureka-webapp/src/main/webapp/eureka/account/views/settings/main/main-controller.js
+++ eureka-webapp/src/main/webapp/eureka/account/views/settings/main/main-controller.js
@@ -13,17 +13,42 @@
         .module('eureka.account')
         .controller('account.settings.MainCtrl', MainCtrl);
         
-    MainCtrl.$inject = ['AccountService', 'users'];
+    MainCtrl.$inject = ['AccountService', 'users', '$mdDialog'];
     
-    function MainCtrl(AccountService, users) {
+    function MainCtrl(AccountService, users, $mdDialog) {
         var vm = this;
+        vm.resetPassword = resetPassword;
 
         getCurrentUser();
+
+        function resetPassword(ev) {
+            $mdDialog.show({
+                controller: ChangePasswordController,
+                templateUrl: 'eureka/account/views/settings/main/modal.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true
+            })
+            .then(function(answer) {
+                console.log('*** Something happended');
+            }, function() {
+                console.log('dialog cancelded');
+            });
+        }
 
         function getCurrentUser() {
             users.getUser().then(function (data) {
                 vm.currentUser = data;
             }, displayError);
+        }
+
+        function ChangePasswordController($scope, $mdDialog) {
+            function hide() {
+                $mdDialog.hide();
+            }
+            function cancel() {
+                $mdDialog.cancel();
+            }
         }
 
         function displayError(msg) {
