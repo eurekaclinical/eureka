d8074b42b1e2aef4e19c5db21095a00d6922f43e
Andrew Post <arpost@emory.edu>
2015-12-03 18:16:10 -0500

    Added dual licensing.

--- eureka-common/src/main/java/edu/emory/cci/aiw/cvrg/eureka/common/filter/SearchFilter.java
+++ eureka-common/src/main/java/edu/emory/cci/aiw/cvrg/eureka/common/filter/SearchFilter.java
@@ -4,8 +4,12 @@ package edu.emory.cci.aiw.cvrg.eureka.common.filter;
  * #%L
  * Eureka Common
  * %%
- * Copyright (C) 2012 - 2013 Emory University
+ * Copyright (C) 2012 - 2015 Emory University
  * %%
+ * This program is dual licensed under the Apache 2 and GPLv3 licenses.
+ * 
+ * Apache License, Version 2.0:
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,6 +21,22 @@ package edu.emory.cci.aiw.cvrg.eureka.common.filter;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * GNU General Public License version 3:
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
 
