0eb584d289d7bb40015061611d1cb25f2e8f9282
Miao Ai <miaoai2010@gmail.com>
2016-01-13 00:32:06 -0500

    Rewrite home Angular controller to fix sporadic loading; add home Angular service

--- eureka-webapp/src/main/webapp/eureka/views/main-controller.js
+++ eureka-webapp/src/main/webapp/eureka/views/main-controller.js
@@ -1,21 +1,20 @@
 var eurekaModule = angular.module('eureka');
 
-eurekaModule.controller('NewsAndFundingController',['$scope', '$sce', function($scope,$sce){
+eurekaModule.controller('NewsAndFundingController',['$scope', '$sce', 'NewsAndFunding', 
+    function($scope,$sce,NewsAndFunding){
             var service = {versionHistory:[],supportedBy:[]};
-            $.getJSON('assets/data/version_history.json', function(data) {
-                     service.versionHistory.push.apply(service.versionHistory,data.versionHistory);
-                           $scope.versionHistory = service.versionHistory; 
-            });
-                     
-            $.getJSON('assets/data/supported_by.json', function(data) {
+                NewsAndFunding.versionHistoryAsync().then(function(d) {                  
+                    $scope.versionHistory = d.data.versionHistory;
+                });
+                                
+                NewsAndFunding.supportedByAsync().then(function(d) {
+                    var data = d.data;          
                     var result = data.supportedBy.slice(0, -1).join('; ');
                     if (data.supportedBy.length > 1) {
                             result += '; and ';
                     }
                     result += data.supportedBy[data.supportedBy.length - 1];
-                    $scope.supportedBy = result;
                     $scope.supportedByHTML = $sce.trustAsHtml(result);
-            });
-            
+                });
         
 }]);
