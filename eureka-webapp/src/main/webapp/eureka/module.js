3e5c74ffb1ef7f793327e3241927617f212d95a0
Andrew Post <arpost@emory.edu>
2017-01-25 23:37:28 -0500

    Add back the registration config.

--- eureka-webapp/src/main/webapp/eureka/module.js
+++ eureka-webapp/src/main/webapp/eureka/module.js
@@ -59,6 +59,7 @@
                     .then(function(response) {
             $rootScope.modes = response.data.appPropertiesModes;
             $rootScope.links = response.data.appPropertiesLinks;
+            $rootScope.registration = response.data.appPropertiesRegistration;
         });
         
        $rootScope.userVerficationPerformed = false;
