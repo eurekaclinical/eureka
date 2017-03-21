f0a8c13827e0d94b8cbc7f5610f62370b2b8c0be
JSONS <jsands79@gmail.com>
2017-03-20 20:12:17 -0400

    Cohort and Phenotype edit is displaying data also phenotype showing type

--- eureka-webapp/src/main/webapp/eureka/phenotypes/views/edit/edit-controller.js
+++ eureka-webapp/src/main/webapp/eureka/phenotypes/views/edit/edit-controller.js
@@ -20,27 +20,44 @@
   function EditCtrl($stateParams, PhenotypeService) {
 
     var vm = this;
+    var currentType = '';
+
+    if ($stateParams.id) {
+
+            PhenotypeService.getPhenotype($stateParams.id).then(function(data) {
+                vm.destination = data;
+                currentType = data.type;
+                setType();
+                //getPhenotypes(data);
+
+            }, displayError);
+
+        }
 
     vm.type = _.startCase($stateParams.type);
     vm.timeUnits = ['minutes', 'hours', 'days'];
-
-    switch ($stateParams.type) {
-      case 'categorization':
+function setType(){
+    switch (currentType) {
+      case 'CATEGORIZATION':
         vm.description = 'This category data element may be used wherever its member data elements are accepted.';
+        vm.name = 'Categorization';
         break;
-      case 'sequence':
+      case 'SEQUENCE':
         vm.description = 'Computes intervals with the same start and stop time as the Main data element below when ' +
           'the temporal relationships below are satisfied.';
+          vm.name = 'Sequence';
         break;
-      case 'frequency':
+      case 'FREQUENCY':
         vm.description = 'Computes an interval over the temporal extent of the intervals contributing to the ' +
           'specified frequency count below.';
+          vm.name = 'Frequency';
         break;
-      case 'value threshold':
+      case 'VALUE_THRESHOLD':
         vm.description = 'Computes intervals corresponding to when the specified thresholds below are present.';
+        vm.name = 'Value Threshold';
         break;
     }
-
+}
     vm.openMenu = function($mdOpenMenu, evt) {
       $mdOpenMenu(evt);
     };
