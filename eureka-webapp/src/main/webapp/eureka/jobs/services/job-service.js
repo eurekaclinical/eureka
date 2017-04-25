799042f0a439aa045f7389c4abc382754a9c2d35
Miao Ai <miaoai2010@gmail.com>
2017-04-25 17:25:36 -0400

    Enhancement#173, Angular-lize job page: display only

--- eureka-webapp/src/main/webapp/eureka/jobs/services/job-service.js
+++ eureka-webapp/src/main/webapp/eureka/jobs/services/job-service.js
@@ -22,12 +22,16 @@
         return ({
             fileUpload: fileUpload,
             getJobs: getJobs,
-            getJob: getJob
+            getJob: getJob,
+            getLatestJobs: getLatestJobs,
+            getDestinations: getDestinations,
+            getSourceConfigs: getSourceConfigs
         });
 
         function handleSuccess(response) {
             return response.data;
         }
+        
         function handleError(response) {
             if (!angular.isObject(response.data) && !response.data) {
                 return ($q.reject('An unknown error occurred.'));
@@ -37,25 +41,47 @@
 
         function fileUpload(sourceId) {
 
-            return $http['post'](dataEndpoint+'/file/upload' + sourceId)
+            return $http.post(dataEndpoint+'/file/upload' + sourceId)
                 .then(handleSuccess, handleError);
 
         }
 
         function getJobs() {
 
-            return $http['post'](dataEndpoint+'/jobs')
+            return $http.get(dataEndpoint+'/jobs')
                 .then(handleSuccess, handleError);
 
         }
 
         function getJob(id) {
 
-            return $http['post'](dataEndpoint+'/job/' + id)
+            return $http.get(dataEndpoint+'/job/' + id)
                 .then(handleSuccess, handleError);
 
         }
+        
+        //get a list of existing jobs, with latest one as the first one
+        function getLatestJobs(){
+            
+            return $http.get(dataEndpoint+'/jobs/latest')
+                .then(handleSuccess, handleError);
+        }
 
+        //get a list of destinations
+        function getDestinations(){
+            
+            return $http.get(dataEndpoint+'/destinations')
+                .then(handleSuccess, handleError);
+            
+        }
+
+        //get a list of sourceConfigs
+        function getSourceConfigs(){
+
+            return $http.get(dataEndpoint+'/sourceconfig')
+                .then(handleSuccess, handleError);
+            
+        }
     }
 
 }());
