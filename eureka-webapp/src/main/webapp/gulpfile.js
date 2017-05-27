ec7a80f1a278c4cb2cf76b8b38aea6f857d91e10
Andrew Post <arpost@emory.edu>
2017-05-26 21:40:20 -0400

    Some cleanup of the web client.

--- eureka-webapp/src/main/webapp/gulpfile.js
+++ eureka-webapp/src/main/webapp/gulpfile.js
@@ -4,8 +4,6 @@ var gulp = require('gulp'),
     inject = require('gulp-inject')
     babel = require("gulp-babel"),
     concat = require("gulp-concat"),
-    //wiredep = require('wiredep').stream,
-    opn = require('opn'),
     gulpDocs = require('gulp-ngdocs'),
     htmlmin = require('gulp-htmlmin'),
     useref = require('gulp-useref'),
@@ -13,19 +11,12 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     cleanCSS = require('gulp-clean-css'),
     templateCache = require('gulp-angular-templatecache'),
-    map = require('map-stream'),
     less = require('gulp-less'),
     es = require('event-stream')
 
 
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-gulp.task('bower', function () {
-  gulp.src('./index.html')
-    //.pipe(wiredep())
-    .pipe(gulp.dest('./'));
-});
 
 gulp.task('ngdocs', ['process-js'], function () {
   var options = {
@@ -37,7 +28,7 @@ gulp.task('ngdocs', ['process-js'], function () {
     .pipe(gulp.dest('./ng-docs'));
 });
 
-gulp.task('test', ['process-js', 'ngdocs', 'bower'], function (done) {
+gulp.task('test', ['process-js', 'ngdocs'], function (done) {
   new Server({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
@@ -47,14 +38,14 @@ gulp.task('test', ['process-js', 'ngdocs', 'bower'], function (done) {
 gulp.task('process-js', ['cache-templates'], function () {
   return gulp.src(['eureka/**/module.js', './build/templates.js', 'eureka/**/*.js', '!eureka/**/*-spec.js'])
     .pipe(sourcemaps.init())
-    .pipe(babel())
+    .pipe(babel({presets: ['es2015']}))
     .pipe(concat('app.js'))
     .pipe(rev())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('inject-hash', ['ngdocs', 'bower'], function () {
+gulp.task('inject-hash', ['ngdocs'], function () {
   var target = gulp.src('./index.html');
   var sources = gulp.src(['./build/app-*.js'], {read: false});
  
@@ -68,13 +59,6 @@ gulp.task('less', function () {
         .pipe(gulp.dest('./build'));
 });
 
-//gulp.task('lint', function() {
-//    gulp.src(['eureka/**/*.js', '!eureka/js/**/*.js'])
-//        .pipe(jshint('.jshintrc'))
-//        .pipe(jshint.reporter('jshint-stylish'))
-//        .pipe(exitOnJshintError());
-//}); */
-
 gulp.task('cache-templates', function () {
   return gulp.src('eureka/**/*.html')
     .pipe(templateCache('templates.js', {
@@ -86,7 +70,7 @@ gulp.task('cache-templates', function () {
 
 gulp.task('compile:html', ['less', 'inject-hash'], function() {
     return gulp.src('build/injected/index.html')
-        .pipe(useref({ searchPath: ['bower_components', 'eureka', 'assets', 'build', '.'] }))
+        .pipe(useref({ searchPath: ['eureka', 'assets', 'build', '.'] }))
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', cleanCSS()))
         .pipe(gulp.dest('build'));
