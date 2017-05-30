a0b31a77d043a2644bae316b8f588e337e29a863
Andrew Post <arpost@emory.edu>
2017-05-29 22:10:22 -0400

    Don’t create a separate templates.js file. Just stream it. Inject the index.html with streams so we don’t have to worry about which app-*.js file is the most recent one. Run ng-docs at the same time as the javascript compilation, and build ng-docs from the source files not the compiled app.js.

--- eureka-webapp/src/main/webapp/gulpfile.js
+++ eureka-webapp/src/main/webapp/gulpfile.js
@@ -7,51 +7,40 @@ var gulp = require('gulp'),
     gulpDocs = require('gulp-ngdocs'),
     uglify = require('gulp-uglify'),
     templateCache = require('gulp-angular-templatecache'),
+    addStream = require('add-stream'),
 
-
-gulp.task('ngdocs', ['process-js'], function () {
-  var options = {
-    title: "Eureka Angular Documentation",
-    html5Mode: false
-  };
-  return gulp.src('build/app-*.js')
-    .pipe(gulpDocs.process(options))
-    .pipe(gulp.dest('./ng-docs'));
-});
-
-gulp.task('test', ['process-js', 'ngdocs'], function (done) {
+gulp.task('test', function (done) {
   new Server({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true
   }, done).start();
 });
 
-gulp.task('cache-templates', function () {
-  return gulp.src('eureka/**/*.html')
-    .pipe(templateCache('templates.js', {
-        module: 'eureka',
-        root: 'eureka/'
-    }))
-    .pipe(gulp.dest('./build'));
-});
-
-gulp.task('process-js', ['cache-templates'], function () {
-  return gulp.src(['eureka/**/module.js', './build/templates.js', 'eureka/**/*.js', '!eureka/**/*-spec.js'])
+gulp.task('compile-app.js', function () {
+  var target = gulp.src('./index.html');
+  var sources = gulp.src(['eureka/**/module.js', 'eureka/**/*.js', '!eureka/**/*-spec.js'])
     .pipe(sourcemaps.init())
+    .pipe(addStream.obj(gulp.src('eureka/**/*.html')
+		.pipe(templateCache('templates.js', {module: 'eureka', root: 'eureka/'}))))
     .pipe(babel({presets: ['es2015']}))
     .pipe(concat('app.js'))
     .pipe(rev())
     .pipe(uglify())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build'));
-});
-
-gulp.task('inject-hash', ['ngdocs'], function () {
-  var target = gulp.src('./index.html');
-  var sources = gulp.src(['./build/app-*.js'], {read: false});
  
     return target.pipe(inject(sources,{relative: true}))
         .pipe(gulp.dest('./build/injected'));
 });
 
-gulp.task('default', ['inject-hash']);
+gulp.task('ngdocs', function () {
+  var options = {
+    title: "Eureka! Clinical Analytics Web Client Documentation",
+    html5Mode: false
+  };
+  return gulp.src(['eureka/**/module.js', 'eureka/**/*.js', '!eureka/**/*-spec.js'])
+    .pipe(gulpDocs.process(options))
+    .pipe(gulp.dest('./ng-docs'));
+});
+
+gulp.task('default', ['ngdocs', 'compile-app.js']);
