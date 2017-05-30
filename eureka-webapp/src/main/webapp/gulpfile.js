7675a5fc5eb6da95c8379c752d234b6af197773d
Andrew Post <arpost@emory.edu>
2017-05-30 09:59:23 -0400

    The eureka/js directory does not exist, so remove that constraint from the source files processed by the lint task.

--- eureka-webapp/src/main/webapp/gulpfile.js
+++ eureka-webapp/src/main/webapp/gulpfile.js
@@ -12,7 +12,7 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
-    gulp.src(['eureka/**/*.js', '!eureka/js/**/*.js'])
+    gulp.src('eureka/**/*.js')
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail', {'ignoreWarning': true}));
