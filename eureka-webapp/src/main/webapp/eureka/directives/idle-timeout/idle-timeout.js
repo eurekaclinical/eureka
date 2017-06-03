49c5aac11a09b97f83bf17ee1942fd0587ce2ab2
Andrew Post <arpost@emory.edu>
2017-06-03 09:32:49 -0400

    Don’t start idleTimeout if we could not get the session properties.

--- eureka-webapp/src/main/webapp/eureka/directives/idle-timeout/idle-timeout.js
+++ eureka-webapp/src/main/webapp/eureka/directives/idle-timeout/idle-timeout.js
@@ -22,12 +22,14 @@
             controllerAs: 'vm',
 			link: function(scope, element, attrs, ctrl) {
                 let sessionProperties = ctrl.getSessionProperties();
-				$(element).idleTimeout({
-					idleTimeLimit: sessionProperties.maxInactiveInterval - 30, //Time out with 30 seconds to spare to make sure the server session doesn't expire first
-					redirectUrl: '/logout',
-					alertDisplayLimit: 60, // Display 60 seconds before send of session.
-					sessionKeepAliveTimer: sessionProperties.maxInactiveInterval - 15 //Send a keep alive signal with 15 seconds to spare.
-				  });
+                if (sessionProperties) {
+					$(element).idleTimeout({
+						idleTimeLimit: sessionProperties.maxInactiveInterval - 30, //Time out with 30 seconds to spare to make sure the server session doesn't expire first
+						redirectUrl: '/logout',
+						alertDisplayLimit: 60, // Display 60 seconds before send of session.
+						sessionKeepAliveTimer: sessionProperties.maxInactiveInterval - 15 //Send a keep alive signal with 15 seconds to spare.
+					  });
+                }
 			}
 		};
     }
