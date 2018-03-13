cb02334d23667c18268ad818ae1eb352b8a73724
gdileep <gdileep@BMI-Post-XPS3.wireless.emory.edu>
2018-03-13 18:10:08 -0400

    updated context.xml

--- eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/entity/SourceConfigEntity.java
+++ eureka-protempa-etl/src/main/java/edu/emory/cci/aiw/cvrg/eureka/etl/entity/SourceConfigEntity.java
@@ -40,7 +40,6 @@ package edu.emory.cci.aiw.cvrg.eureka.etl.entity;
  * #L%
  */
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
